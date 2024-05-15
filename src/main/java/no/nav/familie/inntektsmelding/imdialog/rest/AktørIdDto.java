02c49251a3427465103e890edf48d47e70aa31f8
johannbm <johannes.moskvil@gmail.com>
2024-05-15 16:39:25 +0200

    extract regexp

--- src/main/java/no/nav/familie/inntektsmelding/imdialog/rest/AktørIdDto.java
+++ src/main/java/no/nav/familie/inntektsmelding/imdialog/rest/AktørIdDto.java
@@ -7,5 +7,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record AktørIdDto(
-    @JsonValue @NotNull @Pattern(regexp = "\\d{13}$", message = "aktørId ${validatedValue} har ikke gyldig verdi (pattern '{regexp}')") String aktørId) {
+    @JsonValue @NotNull @Pattern(regexp = VALID_REGEXP, message = "aktørId ${validatedValue} har ikke gyldig verdi (pattern '{regexp}')") String aktørId) {
+    private static final String VALID_REGEXP = "^\\d{13}$";
 }
--- src/main/java/no/nav/familie/inntektsmelding/imdialog/rest/OrganisasjonsnummerDto.java
+++ src/main/java/no/nav/familie/inntektsmelding/imdialog/rest/OrganisasjonsnummerDto.java
@@ -6,5 +6,6 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record OrganisasjonsnummerDto(
-    @JsonValue @NotNull @Pattern(regexp = "\\d{9}$", message = "organisasjonsnummer ${validatedValue} har ikke gyldig verdi (pattern '{regexp}')") String organisasjonsnummer) {
+    @JsonValue @NotNull @Pattern(regexp = VALID_REGEXP, message = "organisasjonsnummer ${validatedValue} har ikke gyldig verdi (pattern '{regexp}')") String organisasjonsnummer) {
+    private static final String VALID_REGEXP = "^\\d{9}$";
 }
