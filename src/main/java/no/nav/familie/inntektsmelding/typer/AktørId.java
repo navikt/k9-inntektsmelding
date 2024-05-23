22c26315c58fe89f92c8891e3673a66c9fd6b576
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-05-23 17:36:24 +0200

    Endrer validering for aktør id

--- src/main/java/no/nav/familie/inntektsmelding/typer/AktørId.java
+++ src/main/java/no/nav/familie/inntektsmelding/typer/AktørId.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
  * DNR til FNR i Folkeregisteret. Tilsvarende vil den kunne referere personer som har ident fra et utenlandsk system.
  */
 public class AktørId {
-    private static final String VALID_REGEXP = "^\\d{9}$";
+    private static final String VALID_REGEXP = "^\\d{20}$";
 
     private static final Pattern VALID = Pattern.compile(VALID_REGEXP, Pattern.CASE_INSENSITIVE);
 
