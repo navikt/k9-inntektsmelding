b5d494c0d5fe4970fe37f711c70e45e19710b3c4
Michal J. Sladek <michal.sladek@nav.no>
2024-05-30 15:03:48 +0200

    Fix: manglende jdbc url

--- src/main/java/no/nav/familie/inntektsmelding/server/JettyServer.java
+++ src/main/java/no/nav/familie/inntektsmelding/server/JettyServer.java
@@ -156,7 +156,7 @@ public class JettyServer {
             var databaseName = ENV.getRequiredProperty("DB_DATABASE");
             return "jdbc:postgresql://" + host + ":" + port + "/" + databaseName;
         }
-        return ENV.getRequiredProperty("DB_URL");
+        return "jdbc:" + ENV.getRequiredProperty("DB_URL");
     }
 
     private void start() throws Exception {
