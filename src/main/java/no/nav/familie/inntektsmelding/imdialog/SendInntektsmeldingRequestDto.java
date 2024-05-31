0d6c8ccb1135a0b5e1f2fa55b15e4d77fafdf41e
Espen Johansen Velsvik <espen.velsvik@nav.no>
2024-05-31 11:35:25 +0200

    Henter forespørsel fra uuid

--- src/main/java/no/nav/familie/inntektsmelding/imdialog/SendInntektsmeldingRequestDto.java
+++ src/main/java/no/nav/familie/inntektsmelding/imdialog/SendInntektsmeldingRequestDto.java
@@ -1,19 +1,19 @@
 package no.nav.familie.inntektsmelding.imdialog;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import no.nav.familie.inntektsmelding.koder.Naturalytelsetype;
-import no.nav.familie.inntektsmelding.koder.Ytelsetype;
 import no.nav.familie.inntektsmelding.typer.YtelseTypeDto;
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.List;
-
-public record SendInntektsmeldingRequestDto(@NotNull @Valid InntektsmeldingDialogRest.AktørIdRequestDto aktorId, @NotNull @Valid YtelseTypeDto ytelse,
+public record SendInntektsmeldingRequestDto(@NotNull @Valid String foresporselUuid,
+                                            @NotNull @Valid InntektsmeldingDialogRest.AktørIdRequestDto aktorId, @NotNull @Valid YtelseTypeDto ytelse,
                                             @NotNull String arbeidsgiverIdent, @NotNull String telefonnummer, @NotNull LocalDate startdato,
                                             @NotNull @Min(0) @Max(Integer.MAX_VALUE) @Digits(integer = 20, fraction = 2) BigDecimal inntekt,
                                             @NotNull List<@Valid RefusjonsperiodeRequestDto> refusjonsperioder,
