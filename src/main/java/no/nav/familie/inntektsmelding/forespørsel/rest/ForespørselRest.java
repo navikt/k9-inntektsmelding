ab135e6c16f1560356059e158ed6a9a21bc73e0c
Michal J. Sladek <michal.sladek@nav.no>
2024-06-05 15:26:47 +0200

    Legger inn ForespørselDto til å inne sende selve entiteten til frontend.

--- src/main/java/no/nav/familie/inntektsmelding/forespørsel/rest/ForespørselRest.java
+++ src/main/java/no/nav/familie/inntektsmelding/forespørsel/rest/ForespørselRest.java
@@ -1,5 +1,6 @@
 package no.nav.familie.inntektsmelding.forespørsel.rest;
 
+import java.time.LocalDate;
 import java.util.UUID;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -14,10 +15,12 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import no.nav.familie.inntektsmelding.forespørsel.modell.ForespørselEntitet;
 import no.nav.familie.inntektsmelding.forespørsel.tjenester.ForespørselBehandlingTjeneste;
 import no.nav.familie.inntektsmelding.forespørsel.tjenester.ForespørselTjeneste;
 import no.nav.familie.inntektsmelding.typer.AktørIdDto;
 import no.nav.familie.inntektsmelding.typer.OrganisasjonsnummerDto;
+import no.nav.familie.inntektsmelding.typer.YtelseTypeDto;
 import no.nav.familie.inntektsmelding.typer.YtelseTypeMapper;
 import no.nav.vedtak.sikkerhet.jaxrs.UtenAutentisering;
 
@@ -41,7 +44,6 @@ public class ForespørselRest {
     }
 
     @POST
-    @UtenAutentisering
     @Consumes(MediaType.APPLICATION_JSON)
     @Operation(description = "Oppretter en forespørsel om inntektsmelding", tags = "forespørsel")
     public Response createForespørsel(OpprettForespørselRequest request) {
@@ -51,12 +53,21 @@ public class ForespørselRest {
     }
 
     @GET
-    @UtenAutentisering
     @Path("/{uuid}")
     @Operation(description = "Henter en forespørsel for gitt UUID", tags = "forespørsel")
     public Response readForespørsel(@PathParam("uuid") UUID forespørselUUID) {
-        return Response.ok(forespørselTjeneste.finnForespørsel(forespørselUUID)).build();
+        return Response.ok(forespørselTjeneste.finnForespørsel(forespørselUUID).map(ForespørselRest::mapTilDto).orElseThrow()).build();
     }
 
+    record ForespørselDto(UUID uuid, OrganisasjonsnummerDto organisasjonsnummer, LocalDate skjæringstidspunkt, AktørIdDto brukerAktørId, YtelseTypeDto ytelseType) {}
+
+    static ForespørselDto mapTilDto(ForespørselEntitet entitet) {
+        return new ForespørselDto(
+            entitet.getUuid(),
+            new OrganisasjonsnummerDto(entitet.getOrganisasjonsnummer()),
+            entitet.getSkjæringstidspunkt(),
+            new AktørIdDto(entitet.getBrukerAktørId()),
+            YtelseTypeMapper.map(entitet.getYtelseType()));
+    }
 }
 
