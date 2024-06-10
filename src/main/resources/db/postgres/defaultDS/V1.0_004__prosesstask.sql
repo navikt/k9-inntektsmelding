bffb16d090811311277dfef4ef8c4fa696ec0535
Petter Ekern <petter.ekern@nav.no>
2024-06-10 16:08:38 +0200

    FIX: Bruker prosesstask db skript fra abakus

--- src/main/resources/db/postgres/defaultDS/V1.0_004__prosesstask.sql
+++ src/main/resources/db/postgres/defaultDS/V1.0_004__prosesstask.sql
@@ -1,78 +1,89 @@
-CREATE TABLE PROSESS_TASK
+create table prosess_task
 (
-    ID                        NUMERIC,
-    TASK_TYPE                 VARCHAR(50),
-    PRIORITET                 NUMERIC(1, 0) DEFAULT 0,
-    STATUS                    VARCHAR(20)   DEFAULT 'KLAR',
-    TASK_PARAMETERE           VARCHAR(4000),
-    TASK_PAYLOAD              TEXT,
-    TASK_GRUPPE               VARCHAR(250),
-    TASK_SEKVENS              VARCHAR(100)  DEFAULT '1',
-    PARTITION_KEY             VARCHAR(4)    DEFAULT to_char(current_date, 'MM'),
-    NESTE_KJOERING_ETTER      TIMESTAMP(0)  DEFAULT (NOW() AT TIME ZONE 'utc'),
-    FEILEDE_FORSOEK           NUMERIC(5, 0) DEFAULT 0,
-    SISTE_KJOERING_TS         TIMESTAMP(6),
-    SISTE_KJOERING_FEIL_KODE  VARCHAR(50),
-    SISTE_KJOERING_FEIL_TEKST TEXT,
-    SISTE_KJOERING_SERVER     VARCHAR(50),
-    SISTE_KJOERING_PLUKK_TS   TIMESTAMP(6),
-    OPPRETTET_AV              VARCHAR(20)   DEFAULT 'VL',
-    OPPRETTET_TID             TIMESTAMP(6)  DEFAULT (NOW() AT TIME ZONE 'utc') NOT NULL,
-    BLOKKERT_AV               NUMERIC                                 NULL,
-    VERSJON                   NUMERIC       DEFAULT 0
-) PARTITION BY LIST (STATUS);
+    id                        numeric                                                                            not null,
+    task_type                 varchar(50)                                                                        not null,
+    prioritet                 numeric(1)   default 0                                                             not null,
+    status                    varchar(20)  default 'KLAR'::character varying                                     not null,
+    task_parametere           varchar(4000),
+    task_payload              text,
+    task_gruppe               varchar(250),
+    task_sekvens              varchar(100) default '1'::character varying                                        not null,
+    partition_key             varchar(4)   default to_char((CURRENT_DATE)::timestamp with time zone, 'MM'::text) not null,
+    neste_kjoering_etter      timestamp(0) default CURRENT_TIMESTAMP,
+    feilede_forsoek           numeric(5)   default 0,
+    siste_kjoering_ts         timestamp(6),
+    siste_kjoering_feil_kode  varchar(50),
+    siste_kjoering_feil_tekst text,
+    siste_kjoering_server     varchar(50),
+    opprettet_av              varchar(20)  default 'VL'::character varying                                       not null,
+    opprettet_tid             timestamp(6) default CURRENT_TIMESTAMP                                             not null,
+    blokkert_av               numeric,
+    versjon                   numeric      default 0                                                             not null,
+    siste_kjoering_slutt_ts   timestamp(6),
+    siste_kjoering_plukk_ts   timestamp(6),
+    constraint pk_prosess_task
+        primary key (id, status, partition_key)
+)
+    partition by LIST (status);
 
-COMMENT ON COLUMN PROSESS_TASK.ID IS 'Primary Key';
-COMMENT ON COLUMN PROSESS_TASK.TASK_TYPE IS 'navn på task. Brukes til å matche riktig implementasjon';
-COMMENT ON COLUMN PROSESS_TASK.PRIORITET IS 'prioritet på task.  Høyere tall har høyere prioritet';
-COMMENT ON COLUMN PROSESS_TASK.STATUS IS 'status på task: KLAR, NYTT_FORSOEK, FEILET, VENTER_SVAR, FERDIG';
-COMMENT ON COLUMN PROSESS_TASK.TASK_PARAMETERE IS 'parametere angitt for en task';
-COMMENT ON COLUMN PROSESS_TASK.TASK_PAYLOAD IS 'inputdata for en task';
-COMMENT ON COLUMN PROSESS_TASK.TASK_GRUPPE IS 'angir en unik id som grupperer flere ';
-COMMENT ON COLUMN PROSESS_TASK.TASK_SEKVENS IS 'angir rekkefølge på task innenfor en gruppe ';
-COMMENT ON COLUMN PROSESS_TASK.NESTE_KJOERING_ETTER IS 'tasken skal ikke kjøeres før tidspunkt er passert';
-COMMENT ON COLUMN PROSESS_TASK.FEILEDE_FORSOEK IS 'antall feilede forsøk';
-COMMENT ON COLUMN PROSESS_TASK.SISTE_KJOERING_TS IS 'siste gang tasken ble forsøkt kjørt';
-COMMENT ON COLUMN PROSESS_TASK.SISTE_KJOERING_FEIL_KODE IS 'siste feilkode tasken fikk';
-COMMENT ON COLUMN PROSESS_TASK.SISTE_KJOERING_FEIL_TEKST IS 'siste feil tasken fikk';
-COMMENT ON COLUMN PROSESS_TASK.SISTE_KJOERING_SERVER IS 'navn på node som sist kjørte en task (server@pid)';
-COMMENT ON COLUMN PROSESS_TASK.SISTE_KJOERING_PLUKK_TS IS 'siste gang tasken ble forsøkt plukket (fra db til in-memory, før kjøring)';
-COMMENT ON COLUMN PROSESS_TASK.VERSJON IS 'angir versjon for optimistisk låsing';
-COMMENT ON COLUMN PROSESS_TASK.BLOKKERT_AV IS 'Id til ProsessTask som blokkerer kjøring av denne (når status=VETO)';
-COMMENT ON TABLE PROSESS_TASK IS 'Inneholder tasks som skal kjøres i bakgrunnen';
+comment on table prosess_task is 'Inneholder tasks som skal kjøres i bakgrunnen';
 
---------------------------------------------------------
---  Constraints for Table PROSESS_TASK
---------------------------------------------------------
-ALTER TABLE PROSESS_TASK
-    ADD CONSTRAINT PK_PROSESS_TASK PRIMARY KEY (ID, STATUS, PARTITION_KEY);
+comment on column prosess_task.id is 'Primary Key';
 
-ALTER TABLE PROSESS_TASK
-    ALTER COLUMN VERSJON SET NOT NULL;
-ALTER TABLE PROSESS_TASK
-    ALTER COLUMN TASK_SEKVENS SET NOT NULL;
-ALTER TABLE PROSESS_TASK
-    ALTER COLUMN STATUS SET NOT NULL;
-ALTER TABLE PROSESS_TASK
-    ALTER COLUMN PRIORITET SET NOT NULL;
-ALTER TABLE PROSESS_TASK
-    ALTER COLUMN TASK_TYPE SET NOT NULL;
-ALTER TABLE PROSESS_TASK
-    ALTER COLUMN ID SET NOT NULL;
+comment on column prosess_task.task_type is 'navn på task. Brukes til å matche riktig implementasjon';
+
+comment on column prosess_task.prioritet is 'prioritet på task.  Høyere tall har høyere prioritet';
+
+comment on column prosess_task.status is 'status på task: KLAR, NYTT_FORSOEK, FEILET, VENTER_SVAR, FERDIG';
+
+comment on column prosess_task.task_parametere is 'parametere angitt for en task';
+
+comment on column prosess_task.task_payload is 'inputdata for en task';
+
+comment on column prosess_task.task_gruppe is 'angir en unik id som grupperer flere ';
+
+comment on column prosess_task.task_sekvens is 'angir rekkefølge på task innenfor en gruppe ';
+
+comment on column prosess_task.neste_kjoering_etter is 'tasken skal ikke kjøeres før tidspunkt er passert';
+
+comment on column prosess_task.feilede_forsoek is 'antall feilede forsøk';
+
+comment on column prosess_task.siste_kjoering_ts is 'siste gang tasken ble forsøkt kjørt (før kjøring)';
+
+comment on column prosess_task.siste_kjoering_feil_kode is 'siste feilkode tasken fikk';
+
+comment on column prosess_task.siste_kjoering_feil_tekst is 'siste feil tasken fikk';
+
+comment on column prosess_task.siste_kjoering_server is 'navn på node som sist kjørte en task (server@pid)';
+
+comment on column prosess_task.blokkert_av is 'Id til ProsessTask som blokkerer kjøring av denne (når status=VETO)';
+
+comment on column prosess_task.versjon is 'angir versjon for optimistisk låsing';
+
+comment on column prosess_task.siste_kjoering_slutt_ts is 'siste gang tasken ble forsøkt plukket (klargjort til kjøring)';
+
+CREATE INDEX IDX_PROSESS_TASK_2
+    ON PROSESS_TASK (TASK_TYPE);
+CREATE INDEX IDX_PROSESS_TASK_3
+    ON PROSESS_TASK (NESTE_KJOERING_ETTER);
+CREATE INDEX IDX_PROSESS_TASK_5
+    ON PROSESS_TASK (TASK_GRUPPE);
+CREATE INDEX IDX_PROSESS_TASK_1
+    ON PROSESS_TASK (STATUS);
+CREATE INDEX IDX_PROSESS_TASK_4
+    ON PROSESS_TASK (ID);
+CREATE INDEX IDX_PROSESS_TASK_7
+    ON PROSESS_TASK (PARTITION_KEY);
+CREATE UNIQUE INDEX UIDX_PROSESS_TASK
+    ON PROSESS_TASK (ID, STATUS, PARTITION_KEY);
 
 CREATE SEQUENCE SEQ_PROSESS_TASK MINVALUE 1000000 START WITH 1000000 INCREMENT BY 50 NO CYCLE;
 CREATE SEQUENCE SEQ_PROSESS_TASK_GRUPPE MINVALUE 10000000 START WITH 10000000 INCREMENT BY 1000000 NO CYCLE;
 
-
-alter table prosess_task
-    add column SISTE_KJOERING_SLUTT_TS timestamp(6);
-
-COMMENT ON COLUMN PROSESS_TASK.SISTE_KJOERING_TS IS 'siste gang tasken ble forsøkt kjørt (før kjøring)';
-COMMENT ON COLUMN PROSESS_TASK.SISTE_KJOERING_SLUTT_TS IS 'tidsstempel siste gang tasken ble kjørt (etter kjøring)';
-
 -- Etablerer et sett med bøtter som ferdig tasks kan legge seg i avhengig av hvilken måned de er opprettet i.
 -- Legger opp til at disse bøttene kan prunes etter kontinuerlig for å bevare ytelsen
-CREATE TABLE PROSESS_TASK_PARTITION_DEFAULT PARTITION OF PROSESS_TASK   DEFAULT;
+CREATE TABLE PROSESS_TASK_PARTITION_DEFAULT PARTITION OF PROSESS_TASK
+    DEFAULT;
 
 CREATE TABLE PROSESS_TASK_PARTITION_FERDIG PARTITION OF PROSESS_TASK
     FOR VALUES IN ('FERDIG') PARTITION BY LIST (PARTITION_KEY);
@@ -101,18 +112,3 @@ CREATE TABLE PROSESS_TASK_PARTITION_FERDIG_11 PARTITION OF PROSESS_TASK_PARTITIO
 CREATE TABLE PROSESS_TASK_PARTITION_FERDIG_12 PARTITION OF PROSESS_TASK_PARTITION_FERDIG
     FOR VALUES IN ('12');
 
-CREATE INDEX IDX_PROSESS_TASK_2
-    ON PROSESS_TASK (TASK_TYPE);
-CREATE INDEX IDX_PROSESS_TASK_3
-    ON PROSESS_TASK (NESTE_KJOERING_ETTER);
-CREATE INDEX IDX_PROSESS_TASK_5
-    ON PROSESS_TASK (TASK_GRUPPE);
-CREATE INDEX IDX_PROSESS_TASK_1
-    ON PROSESS_TASK (STATUS);
-CREATE INDEX IDX_PROSESS_TASK_4
-    ON PROSESS_TASK (ID);
-CREATE INDEX IDX_PROSESS_TASK_7
-    ON PROSESS_TASK (PARTITION_KEY);
-CREATE UNIQUE INDEX UIDX_PROSESS_TASK
-    ON PROSESS_TASK (ID, STATUS, PARTITION_KEY);
-CREATE INDEX IDX_PROSESS_TASK_6 ON PROSESS_TASK (BLOKKERT_AV);
