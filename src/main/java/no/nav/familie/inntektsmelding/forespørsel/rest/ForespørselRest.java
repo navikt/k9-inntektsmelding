75b6b9fa791e607891cdad38e5e9917f19e1e763
anjaaalerud <anja.gotesen.aalerud@nav.no>
2024-06-17 11:39:57 +0200

    POFIM-23 Endrer metodenavn til opprettForespørsel fra createForespørsel

--- src/main/java/no/nav/familie/inntektsmelding/forespørsel/rest/ForespørselRest.java
+++ src/main/java/no/nav/familie/inntektsmelding/forespørsel/rest/ForespørselRest.java
@@ -47,7 +47,7 @@ public class ForespørselRest {
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("/opprett")
     @Operation(description = "Oppretter en forespørsel om inntektsmelding", tags = "forespørsel")
-    public Response createForespørsel(OpprettForespørselRequest request) {
+    public Response opprettForespørsel(OpprettForespørselRequest request) {
         forespørselBehandlingTjeneste.håndterInnkommendeForespørsel(request.skjæringstidspunkt(), KodeverkMapper.mapYtelsetype(request.ytelsetype()),
             new AktørIdEntitet(request.aktørId().id()), new OrganisasjonsnummerDto(request.orgnummer().orgnr()), request.saksnummer());
         return Response.ok().build();
