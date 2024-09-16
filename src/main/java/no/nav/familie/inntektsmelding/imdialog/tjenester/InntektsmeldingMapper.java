fde8d0d9532244dc132f19570af7d252f63a7dd8
anjaaalerud <anja.gotesen.aalerud@nav.no>
2024-09-16 16:05:03 +0200

    POFIM-34 fjerner unused imports

--- src/main/java/no/nav/familie/inntektsmelding/imdialog/tjenester/InntektsmeldingMapper.java
+++ src/main/java/no/nav/familie/inntektsmelding/imdialog/tjenester/InntektsmeldingMapper.java
@@ -8,7 +8,6 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
-import no.nav.familie.inntektsmelding.forespørsel.modell.ForespørselEntitet;
 import no.nav.familie.inntektsmelding.imdialog.modell.BortaltNaturalytelseEntitet;
 import no.nav.familie.inntektsmelding.imdialog.modell.InntektsmeldingEntitet;
 import no.nav.familie.inntektsmelding.imdialog.modell.KontaktpersonEntitet;
--- src/main/java/no/nav/familie/inntektsmelding/integrasjoner/dokgen/RefusjonsendringPeriode.java
+++ src/main/java/no/nav/familie/inntektsmelding/integrasjoner/dokgen/RefusjonsendringPeriode.java
@@ -2,8 +2,6 @@ package no.nav.familie.inntektsmelding.integrasjoner.dokgen;
 
 import java.math.BigDecimal;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-
 public record RefusjonsendringPeriode(String fom, String tom, BigDecimal beloep) {
 
 }
