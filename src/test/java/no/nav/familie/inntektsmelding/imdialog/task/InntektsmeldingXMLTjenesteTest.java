6ead02267dd66df2815cb0fb9327e539f4e55e79
Petter Ekern <petter.ekern@nav.no>
2024-09-19 08:23:49 +0200

    Legger til en test for ordens skyld

--- src/test/java/no/nav/familie/inntektsmelding/imdialog/task/InntektsmeldingXMLTjenesteTest.java
+++ src/test/java/no/nav/familie/inntektsmelding/imdialog/task/InntektsmeldingXMLTjenesteTest.java
@@ -68,10 +68,44 @@ class InntektsmeldingXMLTjenesteTest {
 
         // Assert
         // Kjører replace slik at vi kan lagre XML i mer lesbart format under
-        assertThat(xml).isEqualTo(forventetXml().replaceAll("[\r\n\t]", ""));
+        assertThat(xml).isEqualTo(forventetXmlFp().replaceAll("[\r\n\t]", ""));
     }
 
-    private String forventetXml() {
+    @Test
+    void skal_teste_xml_generering_svp() {
+        // Arrange
+        var opprettetTidspunkt = LocalDateTime.of(2024, 6, 30, 12, 12, 30);
+        var naturalytelse = BortaltNaturalytelseEntitet.builder()
+            .medPeriode(LocalDate.of(2024, 6, 10), Tid.TIDENES_ENDE)
+            .medType(NaturalytelseType.AKSJER_GRUNNFONDSBEVIS_TIL_UNDERKURS)
+            .medMånedBeløp(BigDecimal.valueOf(2000))
+            .build();
+        var aktørIdSøker = new AktørIdEntitet("1234567891234");
+        var fnrSøker = new PersonIdent("11111111111");
+        var inntektsmelding = InntektsmeldingEntitet.builder()
+            .medArbeidsgiverIdent("999999999")
+            .medStartDato(LocalDate.of(2024, 6, 1))
+            .medYtelsetype(Ytelsetype.SVANGERSKAPSPENGER)
+            .medMånedInntekt(BigDecimal.valueOf(35000))
+            .medAktørId(aktørIdSøker)
+            .medMånedRefusjon(BigDecimal.valueOf(35000))
+            .medRefusjonOpphørsdato(Tid.TIDENES_ENDE)
+            .medOpprettetTidspunkt(opprettetTidspunkt)
+            .medKontaktperson(new KontaktpersonEntitet("Test Testen", "111111111"))
+            .medBortfaltNaturalytelser(Collections.singletonList(naturalytelse))
+            .build();
+
+        when(personTjeneste.finnPersonIdentForAktørId(aktørIdSøker)).thenReturn(fnrSøker);
+
+        // Act
+        var xml = inntektsmeldingXMLTjeneste.lagXMLAvInntektsmelding(inntektsmelding);
+
+        // Assert
+        // Kjører replace slik at vi kan lagre XML i mer lesbart format under
+        assertThat(xml).isEqualTo(forventetXmlSVP().replaceAll("[\r\n\t]", ""));
+    }
+
+    private String forventetXmlFp() {
         return """
             <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
             <melding xmlns="http://seres.no/xsd/NAV/Inntektsmelding_M/20181211">
@@ -116,4 +150,49 @@ class InntektsmeldingXMLTjenesteTest {
             </melding>
             """;
     }
+    private String forventetXmlSVP() {
+        return """
+            <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+            <melding xmlns="http://seres.no/xsd/NAV/Inntektsmelding_M/20181211">
+            	<Skjemainnhold>
+            		<ytelse>Svangerskapspenger</ytelse>
+            		<aarsakTilInnsending>Ny</aarsakTilInnsending>
+            		<arbeidsgiver>
+            			<virksomhetsnummer>999999999</virksomhetsnummer>
+            			<kontaktinformasjon>
+            				<kontaktinformasjonNavn>Test Testen</kontaktinformasjonNavn>
+            				<telefonnummer>111111111</telefonnummer>
+            			</kontaktinformasjon>
+            		</arbeidsgiver>
+            		<arbeidstakerFnr>11111111111</arbeidstakerFnr>
+            		<naerRelasjon>false</naerRelasjon>
+            		<arbeidsforhold>
+            			<foersteFravaersdag>2024-06-01</foersteFravaersdag>
+            			<beregnetInntekt>
+            				<beloep>35000</beloep>
+            			</beregnetInntekt>
+            		</arbeidsforhold>
+            		<refusjon>
+            			<refusjonsbeloepPrMnd>35000</refusjonsbeloepPrMnd>
+            			<refusjonsopphoersdato>9999-12-31</refusjonsopphoersdato>
+            			<endringIRefusjonListe/>
+            		</refusjon>
+            		<opphoerAvNaturalytelseListe>
+            			<opphoerAvNaturalytelse>
+            				<naturalytelseType>aksjerGrunnfondsbevisTilUnderkurs</naturalytelseType>
+            				<fom>2024-06-10</fom>
+            				<beloepPrMnd>2000</beloepPrMnd>
+            			</opphoerAvNaturalytelse>
+            		</opphoerAvNaturalytelseListe>
+            		<gjenopptakelseNaturalytelseListe/>
+            		<avsendersystem>
+            			<systemnavn>NAV_NO</systemnavn>
+            			<systemversjon>1.0</systemversjon>
+            			<innsendingstidspunkt>2024-06-30T12:12:30</innsendingstidspunkt>
+            		</avsendersystem>
+            	</Skjemainnhold>
+            </melding>
+            """;
+    }
+
 }
