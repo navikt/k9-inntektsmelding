f7142c1615e5b999682667304055850974c97910
Michal J. Sladek <michal.sladek@nav.no>
2024-10-21 10:30:25 +0200

    Fikser manglende tilgangssjekk.

--- src/main/java/no/nav/familie/inntektsmelding/forespørsel/rest/ForespørselRest.java
+++ src/main/java/no/nav/familie/inntektsmelding/forespørsel/rest/ForespørselRest.java
@@ -105,8 +105,12 @@ public class ForespørselRest {
 
     @POST
     @Path("/sett-til-utgatt")
+    @Tilgangskontrollert
     public Response settForespørselTilUtgått(LukkForespørselRequest request) {
         LOG.info("Setter forespørsel for fagsakSaksnummer {} til utgått", request.fagsakSaksnummer());
+
+        sjekkErSystemkall();
+        
         forespørselBehandlingTjeneste.settForespørselTilUtgått(request.fagsakSaksnummer(), request.orgnummer(), request.skjæringstidspunkt());
         return Response.ok().build();
     }
