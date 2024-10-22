ef01d6d6901ded9b3603c2daad3eb9533fef5b88
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2024-10-22 22:43:52 +0200

    fjerner ubrukt klasse

--- src/main/java/no/nav/familie/inntektsmelding/forespørsel/rest/SettAlleForespørslerTilUtgåttRequest.java
+++
@@ -1,9 +0,0 @@
-package no.nav.familie.inntektsmelding.forespørsel.rest;
-
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-
-import no.nav.familie.inntektsmelding.typer.dto.SaksnummerDto;
-
-public record SettAlleForespørslerTilUtgåttRequest(@NotNull @Valid SaksnummerDto fagsakSaksnummer) {
-}
