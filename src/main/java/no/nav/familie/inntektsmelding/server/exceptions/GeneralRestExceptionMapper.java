ca43c74983222a3024e67d1e03cad523c7129dce
Petter Ekern <petter.ekern@nav.no>
2024-10-23 14:56:02 +0200

    POFIM-80: Eksponerer ikke full feil til frontend

--- src/main/java/no/nav/familie/inntektsmelding/server/exceptions/GeneralRestExceptionMapper.java
+++ src/main/java/no/nav/familie/inntektsmelding/server/exceptions/GeneralRestExceptionMapper.java
@@ -9,7 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
-import no.nav.vedtak.exception.FunksjonellException;
 import no.nav.vedtak.exception.ManglerTilgangException;
 import no.nav.vedtak.felles.jpa.TomtResultatException;
 import no.nav.vedtak.log.mdc.MDCOperations;
@@ -23,44 +22,35 @@ public class GeneralRestExceptionMapper implements ExceptionMapper<Throwable> {
     public Response toResponse(Throwable feil) {
         try {
             if (feil instanceof TomtResultatException) {
-                return handleTomtResultatFeil(getExceptionMelding(feil));
+                return handleTomtResultatFeil("Tomt resultat");
             }
             if (feil instanceof ManglerTilgangException) {
-                return ikkeTilgang(getExceptionMelding(feil));
+                return ikkeTilgang("Mangler tilgang");
             }
             loggTilApplikasjonslogg(feil);
-            return serverError(getExceptionFullFeilmelding(feil));
+            return serverError("Serverfeil");
         } finally {
             MDC.remove("prosess"); //$NON-NLS-1$
         }
     }
 
     private static Response handleTomtResultatFeil(String feilmelding) {
+        var callId = MDCOperations.getCallId();
         return Response.status(Response.Status.NOT_FOUND)
-            .entity(new FeilDto(FeilType.TOMT_RESULTAT_FEIL, feilmelding))
+            .entity(new FeilDto(FeilType.TOMT_RESULTAT_FEIL, feilmelding, callId))
             .type(MediaType.APPLICATION_JSON)
             .build();
     }
 
     private static Response serverError(String feilmelding) {
-        return Response.serverError().entity(new FeilDto(FeilType.GENERELL_FEIL, feilmelding)).type(MediaType.APPLICATION_JSON).build();
+        return Response.serverError().entity(new FeilDto(FeilType.GENERELL_FEIL, feilmelding, MDCOperations.getCallId())).type(MediaType.APPLICATION_JSON).build();
     }
 
     private static Response ikkeTilgang(String feilmelding) {
         return Response.status(Response.Status.FORBIDDEN)
-            .entity(new FeilDto(FeilType.MANGLER_TILGANG_FEIL, feilmelding))
+            .entity(new FeilDto(FeilType.MANGLER_TILGANG_FEIL, feilmelding, MDCOperations.getCallId()))
             .type(MediaType.APPLICATION_JSON)
             .build();
-    }
-
-    private static String getExceptionFullFeilmelding(Throwable feil) {
-        var callId = MDCOperations.getCallId();
-        var feilbeskrivelse = getExceptionMelding(feil);
-        if (feil instanceof FunksjonellException fe) {
-            var løsningsforslag = getTextForField(fe.getLøsningsforslag());
-            return String.format("Det oppstod en feil: %s - %s. Referanse-id: %s", feilbeskrivelse, løsningsforslag, callId);
-        }
-        return String.format("Det oppstod en serverfeil: %s. Meld til support med referanse-id: %s", feilbeskrivelse, callId);
     }
 
     private static void loggTilApplikasjonslogg(Throwable feil) {
@@ -75,5 +65,4 @@ public class GeneralRestExceptionMapper implements ExceptionMapper<Throwable> {
     private static String getTextForField(String input) {
         return input != null ? LoggerUtils.removeLineBreaks(input) : "";
     }
-
 }
