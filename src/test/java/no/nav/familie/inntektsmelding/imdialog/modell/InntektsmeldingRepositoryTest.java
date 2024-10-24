acec06a243e777e292128f4310f762b541271dbd
Michal J. Sladek <michal.sladek@nav.no>
2024-10-24 22:08:02 +0200

    Forbedrer testene

--- src/test/java/no/nav/familie/inntektsmelding/imdialog/modell/InntektsmeldingRepositoryTest.java
+++ src/test/java/no/nav/familie/inntektsmelding/imdialog/modell/InntektsmeldingRepositoryTest.java
@@ -88,9 +88,9 @@ class InntektsmeldingRepositoryTest extends EntityManagerAwareTest {
         assertThat(etterLagring.get().getOpphørsdatoRefusjon()).isEqualTo(førLagring.getOpphørsdatoRefusjon());
         assertThat(etterLagring.get().getMånedRefusjon()).isEqualByComparingTo(førLagring.getMånedRefusjon());
         assertThat(etterLagring.get().getRefusjonsendringer()).hasSize(1);
-        assertThat(etterLagring.get().getRefusjonsendringer().getFirst().getFom()).isEqualTo(førLagring.getRefusjonsendringer().get(0).getFom());
+        assertThat(etterLagring.get().getRefusjonsendringer().getFirst().getFom()).isEqualTo(førLagring.getRefusjonsendringer().getFirst().getFom());
         assertThat(etterLagring.get().getRefusjonsendringer().getFirst().getRefusjonPrMnd()).isEqualByComparingTo(førLagring.getRefusjonsendringer()
-            .get(0)
+            .getFirst()
             .getRefusjonPrMnd());
     }
 
