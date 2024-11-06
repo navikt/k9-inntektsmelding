19dee208fe9c927c3a34ca124b34a1541dc7a737
inga <inga.kristine.holen@nav.no>
2024-11-06 10:17:19 +0100

    Revert "TSFF-962: Helsesjekken må ha chache control, ellers oppdager ikke docker compose at den blir klar" Pusha ved et uhell rett på master, ruller tilbake This reverts commit f0de9b0b1c41037d252a700a006c5dd158e17c5d.

--- src/main/java/no/nav/familie/inntektsmelding/server/app/internal/rest/HealtCheckRest.java
+++ src/main/java/no/nav/familie/inntektsmelding/server/app/internal/rest/HealtCheckRest.java
@@ -4,15 +4,12 @@ import static jakarta.ws.rs.core.Response.Status.SERVICE_UNAVAILABLE;
 
 import java.util.List;
 
-import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Any;
 import jakarta.enterprise.inject.Instance;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.CacheControl;
-import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import org.slf4j.Logger;
@@ -27,20 +24,10 @@ import no.nav.vedtak.log.metrics.ReadinessAware;
 public class HealtCheckRest {
 
     private static final Logger LOG = LoggerFactory.getLogger(HealtCheckRest.class);
-    private static final CacheControl cacheControl = noCache();
 
     private List<LivenessAware> live;
     private List<ReadinessAware> ready;
     private ApplicationServiceStarter starter;
-
-    private static CacheControl noCache() {
-        var cc = new CacheControl();
-        cc.setMustRevalidate(true);
-        cc.setPrivate(true);
-        cc.setNoCache(true);
-        cc.setNoStore(true);
-        return cc;
-    }
 
     @Inject
     public HealtCheckRest(ApplicationServiceStarter starter, @Any Instance<LivenessAware> live, @Any Instance<ReadinessAware> ready) {
@@ -56,27 +43,21 @@ public class HealtCheckRest {
     @GET
     @Path("/isAlive")
     public Response isAlive() {
-        Response.ResponseBuilder builder;
         if (live.stream().allMatch(LivenessAware::isAlive)) {
-            builder = Response.ok("OK", MediaType.TEXT_PLAIN_TYPE);
-        } else {
-            builder = Response.serverError();
+            return Response.ok().build();
         }
         LOG.info("/isAlive NOK.");
-        return builder.cacheControl(cacheControl).build();
+        return Response.serverError().build();
     }
 
     @GET
     @Path("/isReady")
     public Response isReady() {
-        Response.ResponseBuilder builder;
         if (ready.stream().allMatch(ReadinessAware::isReady)) {
-            builder = Response.ok("OK", MediaType.TEXT_PLAIN_TYPE);
-        } else {
-            builder = Response.status(SERVICE_UNAVAILABLE);
+            return Response.ok().build();
         }
         LOG.info("/isReady NOK.");
-        return builder.cacheControl(cacheControl).build();
+        return Response.status(SERVICE_UNAVAILABLE).build();
     }
 
     @GET
