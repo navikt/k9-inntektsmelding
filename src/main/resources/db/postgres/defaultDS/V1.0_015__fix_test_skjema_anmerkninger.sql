daa9f3cd37755fe218f0a2e23213e6f27e7d30ee
Michal J. Sladek <michal.sladek@nav.no>
2024-11-19 22:52:43 +0100

    Rydder sql

--- src/main/resources/db/postgres/defaultDS/V1.0_015__fix_test_skjema_anmerkninger.sql
+++ src/main/resources/db/postgres/defaultDS/V1.0_015__fix_test_skjema_anmerkninger.sql
@@ -1,13 +1,13 @@
 
-COMMENT ON COLUMN FORESPOERSEL.FORSTE_UTTAKSDATO IS 'Dat med første frævarsdag.';
-COMMENT ON COLUMN FORESPOERSEL.STATUS IS 'Status på forespørselen.';
+COMMENT ON COLUMN forespoersel.FORSTE_UTTAKSDATO IS 'Dat med første frævarsdag.';
+COMMENT ON COLUMN forespoersel.STATUS IS 'Status på forespørselen.';
 
-ALTER TABLE FORESPOERSEL RENAME CONSTRAINT "forespoersel_pkey" TO "pk_foresporsel_id";
-ALTER TABLE REFUSJON_ENDRING RENAME CONSTRAINT "refusjon_endring_pkey" TO "pk_refusjon_endring_id";
-ALTER TABLE BORTFALT_NATURALYTELSE RENAME CONSTRAINT "bortfalt_naturalytelse_pkey" TO "pk_bortfalt_naturalytelse_id";
-ALTER TABLE ENDRINGSAARSAK RENAME CONSTRAINT "endringsaarsak_pkey" TO "pk_endringsaarsak_id";
-ALTER TABLE KONTAKTPERSON RENAME CONSTRAINT "kontaktperson_pkey" TO "pk_kontaktperson_id";
-ALTER TABLE INNTEKTSMELDING RENAME CONSTRAINT "inntektsmelding_pkey" TO "pk_inntektsmelding_id";
+ALTER TABLE forespoersel RENAME CONSTRAINT "forespoersel_pkey" TO "pk_foresporsel_id";
+ALTER TABLE refusjon_endring RENAME CONSTRAINT "refusjon_endring_pkey" TO "pk_refusjon_endring_id";
+ALTER TABLE bortfalt_naturalytelse RENAME CONSTRAINT "bortfalt_naturalytelse_pkey" TO "pk_bortfalt_naturalytelse_id";
+ALTER TABLE endringsaarsak RENAME CONSTRAINT "endringsaarsak_pkey" TO "pk_endringsaarsak_id";
+ALTER TABLE kontaktperson RENAME CONSTRAINT "kontaktperson_pkey" TO "pk_kontaktperson_id";
+ALTER TABLE inntektsmelding RENAME CONSTRAINT "inntektsmelding_pkey" TO "pk_inntektsmelding_id";
 
 DROP INDEX idx_bortfalt_naturalytelse_primary_key;
 DROP INDEX idx_endringsaarsak_primary_key;
@@ -16,10 +16,10 @@ DROP INDEX idx_inntektsmelding_primary_key;
 DROP INDEX idx_refusjon_endring_primary_key;
 DROP INDEX idx_kontaktperson_primary_key;
 
-CREATE INDEX IDX_KONTAKTPERSON_INNTEKTSMELDING_ID ON KONTAKTPERSON(INNTEKTSMELDING_ID);
-CREATE INDEX IDX_BORTFALT_NATURALYTELSE_INNTEKTSMELDING_ID ON BORTFALT_NATURALYTELSE(INNTEKTSMELDING_ID);
-CREATE INDEX IDX_ENDRINGSAARSAK_INNTEKTSMELDING_ID ON ENDRINGSAARSAK(INNTEKTSMELDING_ID);
-CREATE INDEX IDX_REFUSJON_ENDRING_INNTEKTSMELDING_ID ON REFUSJON_ENDRING(INNTEKTSMELDING_ID);
+CREATE INDEX idx_kontaktperson_inntektsmelding_id ON kontaktperson(inntektsmelding_id);
+CREATE INDEX idx_bortfalt_naturalytelse_inntektsmelding_id ON bortfalt_naturalytelse(inntektsmelding_id);
+CREATE INDEX idx_endringsaarsak_inntektsmelding_id ON endringsaarsak(inntektsmelding_id);
+CREATE INDEX idx_refusjon_endring_inntektsmelding_id ON refusjon_endring(inntektsmelding_id);
 
-ALTER INDEX idx_forespoersel_uuid RENAME TO uidx_forespoersel_uuid;
+ALTER INDEX idx_forespoersel_uuid RENAME TO "uidx_forespoersel_uuid";
 
