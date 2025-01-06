0eca15218eb6a3c86e00389dc8ca4e7b27ce8412
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2025-01-06 14:18:45 +0100

    Fikser bygg hvor static mock ble brukt uten å lukke den korrekt (#312)

--- src/test/java/no/nav/familie/inntektsmelding/database/JpaExtension.java
+++ src/test/java/no/nav/familie/inntektsmelding/database/JpaExtension.java
@@ -1,5 +1,7 @@
 package no.nav.familie.inntektsmelding.database;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -7,13 +9,18 @@ import no.nav.foreldrepenger.konfig.Environment;
 import no.nav.vedtak.felles.testutilities.db.EntityManagerAwareExtension;
 
 public class JpaExtension extends EntityManagerAwareExtension {
+    private static final Logger LOG = LoggerFactory.getLogger(JpaExtension.class);
     private static final String TEST_DB_CONTAINER = Environment.current().getProperty("testcontainer.test.db", String.class, "postgres:17-alpine");
     private static final PostgreSQLContainer TEST_DATABASE;
 
     static {
-        TEST_DATABASE = new PostgreSQLContainer<>(DockerImageName.parse(TEST_DB_CONTAINER))
-            .withReuse(true);
-        TEST_DATABASE.start();
-        TestDatabaseInit.settOppDatasourceOgMigrer(TEST_DATABASE.getJdbcUrl(), TEST_DATABASE.getUsername(), TEST_DATABASE.getPassword());
+        try {
+            TEST_DATABASE = new PostgreSQLContainer<>(DockerImageName.parse("postgres:17-alpine")).withReuse(true);
+            TEST_DATABASE.start();
+            TestDatabaseInit.settOppDatasourceOgMigrer(TEST_DATABASE.getJdbcUrl(), TEST_DATABASE.getUsername(), TEST_DATABASE.getPassword());
+        } catch (Exception e) {
+            LOG.error("FEIL I TESTCONTAINER!",e);
+            throw new RuntimeException(e);
+        }
     }
 }
