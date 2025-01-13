dc7030cc47852797d560c42dc4e5b82210c5b232
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-01-13 14:43:12 +0100

    Fjern tilgangs-avhengighet, da vi kontrollerer tilgang på en annen måte

--- src/main/java/no/nav/familie/inntektsmelding/refusjonomsorgsdagerarbeidsgiver/rest/RefusjonOmsorgsdagerArbeidsgiverRest.java
+++ src/main/java/no/nav/familie/inntektsmelding/refusjonomsorgsdagerarbeidsgiver/rest/RefusjonOmsorgsdagerArbeidsgiverRest.java
@@ -2,11 +2,6 @@ package no.nav.familie.inntektsmelding.refusjonomsorgsdagerarbeidsgiver.rest;
 
 import static no.nav.familie.inntektsmelding.refusjonomsorgsdagerarbeidsgiver.rest.RefusjonOmsorgsdagerArbeidsgiverRest.BASE_PATH;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -18,6 +13,12 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import no.nav.familie.inntektsmelding.refusjonomsorgsdagerarbeidsgiver.tjenester.ArbeidstakerTjeneste;
 import no.nav.familie.inntektsmelding.server.auth.api.AutentisertMedTokenX;
 import no.nav.familie.inntektsmelding.server.auth.api.Tilgangskontrollert;
@@ -52,6 +53,7 @@ public class RefusjonOmsorgsdagerArbeidsgiverRest {
             @Parameter(description = "Datapakke som inneholder fødselsnummeret til en arbeidstaker") @NotNull @Valid SlåOppArbeidstakerDto slåOppArbeidstakerDto) {
 
         LOG.info("Slår opp arbeidstaker med fødselsnummer {}", slåOppArbeidstakerDto.fødselsnummer());
+
         var dto = arbeidstakerTjeneste.slåOppArbeidstaker(slåOppArbeidstakerDto.fødselsnummer(), slåOppArbeidstakerDto.ytelseType());
         if (dto == null) {
             throw new NotFoundException();
--- src/test/java/no/nav/familie/inntektsmelding/refusjonomsorgsdagerarbeidsgiver/rest/RefusjonOmsorgsdagerArbeidsgiverRestTest.java
+++ src/test/java/no/nav/familie/inntektsmelding/refusjonomsorgsdagerarbeidsgiver/rest/RefusjonOmsorgsdagerArbeidsgiverRestTest.java
@@ -6,8 +6,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import jakarta.ws.rs.NotFoundException;
-
-import no.nav.familie.inntektsmelding.koder.Ytelsetype;
+import jakarta.ws.rs.core.Response;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,45 +14,45 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import jakarta.ws.rs.core.Response;
 import no.nav.familie.inntektsmelding.integrasjoner.person.PersonIdent;
+import no.nav.familie.inntektsmelding.koder.Ytelsetype;
 import no.nav.familie.inntektsmelding.refusjonomsorgsdagerarbeidsgiver.tjenester.ArbeidstakerTjeneste;
 
 @ExtendWith(MockitoExtension.class)
 class RefusjonOmsorgsdagerArbeidsgiverRestTest {
-  @Mock
-  private ArbeidstakerTjeneste arbeidstakerTjeneste;
+    @Mock
+    private ArbeidstakerTjeneste arbeidstakerTjenesteMock;
 
-  private RefusjonOmsorgsdagerArbeidsgiverRest rest;
+    private RefusjonOmsorgsdagerArbeidsgiverRest rest;
 
-  @BeforeEach
-  void setUp() {
-    rest = new RefusjonOmsorgsdagerArbeidsgiverRest(arbeidstakerTjeneste);
-  }
+    @BeforeEach
+    void setUp() {
+        rest = new RefusjonOmsorgsdagerArbeidsgiverRest(arbeidstakerTjenesteMock);
+    }
 
-  @Test
-  void slå_opp_arbeidstaker_skal_returnere_ok_response_når_arbeidstaker_finnes() {
-    var fnr = PersonIdent.fra("12345678910");
-    var dto = new SlåOppArbeidstakerDto(fnr, Ytelsetype.OMSORGSPENGER);
-    var arbeidstakerInfo = new SlåOppArbeidstakerResponseDto("fornavn", "mellomnavn", "etternavn", null);
+    @Test
+    void slå_opp_arbeidstaker_skal_returnere_ok_response_når_arbeidstaker_finnes() {
+        var fnr = PersonIdent.fra("12345678910");
+        var dto = new SlåOppArbeidstakerDto(fnr, Ytelsetype.OMSORGSPENGER);
+        var arbeidstakerInfo = new SlåOppArbeidstakerResponseDto("fornavn", "mellomnavn", "etternavn", null);
 
-    when(arbeidstakerTjeneste.slåOppArbeidstaker(fnr, Ytelsetype.OMSORGSPENGER)).thenReturn(arbeidstakerInfo);
+        when(arbeidstakerTjenesteMock.slåOppArbeidstaker(fnr, Ytelsetype.OMSORGSPENGER)).thenReturn(arbeidstakerInfo);
 
-    Response response = rest.slåOppArbeidstaker(dto);
+        Response response = rest.slåOppArbeidstaker(dto);
 
-    assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
-    assertEquals(arbeidstakerInfo, response.getEntity());
-    verify(arbeidstakerTjeneste).slåOppArbeidstaker(fnr, Ytelsetype.OMSORGSPENGER);
-  }
+        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+        assertEquals(arbeidstakerInfo, response.getEntity());
+        verify(arbeidstakerTjenesteMock).slåOppArbeidstaker(fnr, Ytelsetype.OMSORGSPENGER);
+    }
 
-  @Test
-  void slå_opp_arbeidstaker_skal_kaste_not_found_exception_når_arbeidstaker_ikke_finnes() {
-    var fnr = PersonIdent.fra("12345678910");
-    var dto = new SlåOppArbeidstakerDto(fnr, Ytelsetype.OMSORGSPENGER);
+    @Test
+    void slå_opp_arbeidstaker_skal_kaste_not_found_exception_når_arbeidstaker_ikke_finnes() {
+        var fnr = PersonIdent.fra("12345678910");
+        var dto = new SlåOppArbeidstakerDto(fnr, Ytelsetype.OMSORGSPENGER);
 
-    when(arbeidstakerTjeneste.slåOppArbeidstaker(fnr, Ytelsetype.OMSORGSPENGER)).thenReturn(null);
+        when(arbeidstakerTjenesteMock.slåOppArbeidstaker(fnr, Ytelsetype.OMSORGSPENGER)).thenReturn(null);
 
-    assertThrows(NotFoundException.class, () -> rest.slåOppArbeidstaker(dto));
-    verify(arbeidstakerTjeneste).slåOppArbeidstaker(fnr, Ytelsetype.OMSORGSPENGER);
-  }
+        assertThrows(NotFoundException.class, () -> rest.slåOppArbeidstaker(dto));
+        verify(arbeidstakerTjenesteMock).slåOppArbeidstaker(fnr, Ytelsetype.OMSORGSPENGER);
+    }
 }
