46e40da11c2ae1ac7a8903c1cb60d599aae12696
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-01-14 14:09:31 +0100

    Fjern unødvendige jackson-annotasjoner

--- src/main/java/no/nav/familie/inntektsmelding/integrasjoner/aareg/dto/ArbeidsforholdDto.java
+++ src/main/java/no/nav/familie/inntektsmelding/integrasjoner/aareg/dto/ArbeidsforholdDto.java
@@ -1,35 +1,20 @@
 package no.nav.familie.inntektsmelding.integrasjoner.aareg.dto;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.util.List;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)
-@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record ArbeidsforholdDto(
-    @JsonProperty("arbeidsforholdId")
     String arbeidsforholdId,
-    
-    @JsonProperty("navArbeidsforholdId")
+
     Long navArbeidsforholdId,
-    
-    @JsonProperty("arbeidsgiver")
+
     OpplysningspliktigArbeidsgiverDto arbeidsgiver,
-    
-    @JsonProperty("ansettelsesperiode")
+
     AnsettelsesperiodeDto ansettelsesperiode,
-    
-    @JsonProperty("arbeidsavtaler")
+
     List<ArbeidsavtaleDto> arbeidsavtaler,
-    
-    @JsonProperty("permisjonPermitteringer")
+
     List<PermisjonPermitteringDto> permisjonPermitteringer,
-    
-    @JsonProperty("type")
+
     String type // (kodeverk: Arbeidsforholdtyper)
 ) {
     public List<ArbeidsavtaleDto> arbeidsavtaler() {
