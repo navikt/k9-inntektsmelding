8d5dd94b2b6fed690720247c9736e57b1aff347c
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2025-01-14 14:15:39 +0100

    Fjern unødvendig konstruktør

--- src/main/java/no/nav/familie/inntektsmelding/refusjonomsorgsdagerarbeidsgiver/rest/SlåOppArbeidstakerDto.java
+++ src/main/java/no/nav/familie/inntektsmelding/refusjonomsorgsdagerarbeidsgiver/rest/SlåOppArbeidstakerDto.java
@@ -7,7 +7,5 @@ import no.nav.familie.inntektsmelding.integrasjoner.person.PersonIdent;
 import no.nav.familie.inntektsmelding.koder.Ytelsetype;
 
 public record SlåOppArbeidstakerDto(@Valid @NotNull PersonIdent fødselsnummer, @Valid @NotNull Ytelsetype ytelseType) {
-    public SlåOppArbeidstakerDto {
-    }
 }
 
