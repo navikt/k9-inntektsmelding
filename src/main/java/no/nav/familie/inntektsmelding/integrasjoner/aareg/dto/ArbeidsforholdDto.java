df05f346730ae7bd442d2af66d5d06ff56dd1d76
johannbm <johannes.moskvil@gmail.com>
2025-01-21 10:16:33 +0100

    Legg til annotasjoner for aaregDto

--- src/main/java/no/nav/familie/inntektsmelding/integrasjoner/aareg/dto/ArbeidsforholdDto.java
+++ src/main/java/no/nav/familie/inntektsmelding/integrasjoner/aareg/dto/ArbeidsforholdDto.java
@@ -1,7 +1,12 @@
 package no.nav.familie.inntektsmelding.integrasjoner.aareg.dto;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public record ArbeidsforholdDto(
     String arbeidsforholdId,
     Long navArbeidsforholdId,
