022fcda562bf2a21a130d1d65141ed10bb9c9918
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2025-02-07 14:04:32 +0100

    Starter TaskManager

--- src/main/java/no/nav/familie/inntektsmelding/server/ServiceStarterListener.java
+++ src/main/java/no/nav/familie/inntektsmelding/server/ServiceStarterListener.java
@@ -4,15 +4,21 @@ import jakarta.enterprise.inject.spi.CDI;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 
+import no.nav.k9.prosesstask.impl.TaskManager;
+
 public class ServiceStarterListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         CDI.current().select(ApplicationServiceStarter.class).get().startServices();
+        var taskManager = CDI.current().select(TaskManager.class).get();
+        taskManager.start();
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         CDI.current().select(ApplicationServiceStarter.class).get().stopServices();
+        var taskManager = CDI.current().select(TaskManager.class).get();
+        taskManager.stop();
     }
 }
