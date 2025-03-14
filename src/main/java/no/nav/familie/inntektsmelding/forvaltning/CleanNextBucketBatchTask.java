5830d9ccd46b7a9394ec26b55f05b0e7775831fe
oyvemb <107673448+oyvemb@users.noreply.github.com>
2025-03-14 13:33:43 +0100

    Endrer til "saksnummer" i v1-endepunkter (#408)
    * Endrer til "saksnummer" i v1-endepunkter
    
    * Sniker inn en annen endring - tasken logger alltid 0

--- src/main/java/no/nav/familie/inntektsmelding/forvaltning/CleanNextBucketBatchTask.java
+++ src/main/java/no/nav/familie/inntektsmelding/forvaltning/CleanNextBucketBatchTask.java
@@ -3,9 +3,6 @@ package no.nav.familie.inntektsmelding.forvaltning;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import no.nav.vedtak.felles.prosesstask.api.ProsessTask;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskData;
 import no.nav.vedtak.felles.prosesstask.api.ProsessTaskHandler;
@@ -15,7 +12,6 @@ import no.nav.vedtak.felles.prosesstask.api.ProsessTaskTjeneste;
 @ProsessTask(value = "partition.cleanBucket", prioritet = 3, cronExpression = "0 0 0 1 * *", maxFailedRuns = 1)
 public class CleanNextBucketBatchTask implements ProsessTaskHandler {
 
-    private static final Logger LOG = LoggerFactory.getLogger(CleanNextBucketBatchTask.class);
     private final ProsessTaskTjeneste taskTjeneste;
 
     @Inject
@@ -25,7 +21,6 @@ public class CleanNextBucketBatchTask implements ProsessTaskHandler {
 
     @Override
     public void doTask(ProsessTaskData prosessTaskData) {
-        var antallSlettet = taskTjeneste.tømNestePartisjon();
-        LOG.info("Tømmer neste partisjon med ferdige tasks, slettet {}", antallSlettet); //TODO sjekk om denne faktisk logger antall slettede. Tror truncate alltid returnerer 0.
+        taskTjeneste.tømNestePartisjon();
     }
 }
