7fcf387a787114a115deba9ffe29fc6b9383e371
Helga.Nyrud <helga.nyrud@nav.no>
2025-04-02 11:20:02 +0200

    Legger til perioder i tilleggsinfo for refusjonskrav omsorgspenger

--- src/test/java/no/nav/familie/inntektsmelding/forespørsel/tjenester/ForespørselTeksterTest.java
+++ src/test/java/no/nav/familie/inntektsmelding/forespørsel/tjenester/ForespørselTeksterTest.java
@@ -3,8 +3,15 @@ package no.nav.familie.inntektsmelding.forespørsel.tjenester;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import no.nav.familie.inntektsmelding.imdialog.modell.DelvisFraværsPeriodeEntitet;
+import no.nav.familie.inntektsmelding.imdialog.modell.FraværsPeriodeEntitet;
+
+import no.nav.familie.inntektsmelding.imdialog.modell.PeriodeEntitet;
 
 import org.junit.jupiter.api.Test;
 
@@ -45,6 +52,27 @@ class ForespørselTeksterTest {
         LocalDate førsteFraværsdag = LocalDate.now();
         String statusTekst = ForespørselTekster.lagTilleggsInformasjon(LukkeÅrsak.ORDINÆR_INNSENDING, førsteFraværsdag);
         var forventetTekst = String.format("For første fraværsdag %s", førsteFraværsdag.format(DateTimeFormatter.ofPattern("dd.MM.yy")));
+        assertEquals(forventetTekst, statusTekst);
+    }
+
+    @Test
+    void lagTilleggsInformasjon_OmsorgspengerRefusjon() {
+        List<FraværsPeriodeEntitet> fraværsPerioder = List.of(
+            new FraværsPeriodeEntitet(PeriodeEntitet.fraOgMedTilOgMed(LocalDate.now().minusDays(8), LocalDate.now().minusDays(6))),
+            new FraværsPeriodeEntitet(PeriodeEntitet.fraOgMedTilOgMed(LocalDate.now().minusDays(4), LocalDate.now().minusDays(2))));
+        List<DelvisFraværsPeriodeEntitet> delvisFravær = List.of(
+            new DelvisFraværsPeriodeEntitet(LocalDate.now().minusDays(10), BigDecimal.valueOf(2)),
+            new DelvisFraværsPeriodeEntitet(LocalDate.now().minusDays(1), BigDecimal.valueOf(4)));
+        String statusTekst = ForespørselTekster.lagTilleggsInformasjonForOmsorgspengerRefusjon(fraværsPerioder, delvisFravær);
+        var forventetTekst = String.format("For %s, %s, %s, %s, %s, %s, %s, %s.",
+            delvisFravær.getFirst().getDato().format(DateTimeFormatter.ofPattern("dd.MM.yy")),
+            fraværsPerioder.getFirst().getPeriode().getFom().format(DateTimeFormatter.ofPattern("dd.MM.yy")),
+            fraværsPerioder.getFirst().getPeriode().getFom().plusDays(1).format(DateTimeFormatter.ofPattern("dd.MM.yy")),
+            fraværsPerioder.getFirst().getPeriode().getTom().format(DateTimeFormatter.ofPattern("dd.MM.yy")),
+            fraværsPerioder.getLast().getPeriode().getFom().format(DateTimeFormatter.ofPattern("dd.MM.yy")),
+            fraværsPerioder.getLast().getPeriode().getFom().plusDays(1).format(DateTimeFormatter.ofPattern("dd.MM.yy")),
+            fraværsPerioder.getLast().getPeriode().getTom().format(DateTimeFormatter.ofPattern("dd.MM.yy")),
+            delvisFravær.getLast().getDato().format(DateTimeFormatter.ofPattern("dd.MM.yy")));
         assertEquals(forventetTekst, statusTekst);
     }
 
