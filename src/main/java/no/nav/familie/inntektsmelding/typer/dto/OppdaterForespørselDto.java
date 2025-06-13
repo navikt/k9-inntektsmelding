b89db7cdbca14f4102ab05440f3f1216c040715b
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2025-06-13 11:17:47 +0200

    Legg til konstruktør for opprettelse av OppdaterForespørselDto uten etterspurtePerioder

--- src/main/java/no/nav/familie/inntektsmelding/typer/dto/OppdaterForespørselDto.java
+++ src/main/java/no/nav/familie/inntektsmelding/typer/dto/OppdaterForespørselDto.java
@@ -10,4 +10,10 @@ public record OppdaterForespørselDto(@NotNull LocalDate skjæringstidspunkt,
                                      @NotNull @Valid OrganisasjonsnummerDto orgnr,
                                      @NotNull ForespørselAksjon aksjon,
                                      @Valid List<PeriodeDto> etterspurtePerioder) {
+
+    public OppdaterForespørselDto(@NotNull LocalDate skjæringstidspunkt,
+                                  @NotNull @Valid OrganisasjonsnummerDto orgnr,
+                                  @NotNull ForespørselAksjon aksjon) {
+        this(skjæringstidspunkt, orgnr, aksjon, null);
+    }
 }
