27b7f61a7be8890fc1909c5c00c1dd093da9a286
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2025-06-24 08:17:15 +0200

    Revert "Reapply TSFF-1577: Opprett forespørsel med etterspurte perioder for omsorgspenger men uten mapping til frontend dto (#477)"
    This reverts commit f8ecda4e2f2f51507261d50f894606a884c2fe2b.

--- src/main/java/no/nav/familie/inntektsmelding/imdialog/rest/InntektsmeldingDialogDto.java
+++ src/main/java/no/nav/familie/inntektsmelding/imdialog/rest/InntektsmeldingDialogDto.java
@@ -10,7 +10,6 @@ import jakarta.validation.constraints.NotNull;
 
 import no.nav.familie.inntektsmelding.typer.dto.ForespørselStatusDto;
 import no.nav.familie.inntektsmelding.typer.dto.MånedslønnStatus;
-import no.nav.familie.inntektsmelding.typer.dto.PeriodeDto;
 import no.nav.familie.inntektsmelding.typer.dto.YtelseTypeDto;
 
 public record InntektsmeldingDialogDto(@Valid @NotNull PersonInfoResponseDto person,
