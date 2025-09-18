1850dfb3af4fc172bedcb4213f0114a72e59d7b3
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2025-09-18 12:45:27 +0200

    Lag K9SakTjeneste

---
+++ src/main/java/no/nav/familie/inntektsmelding/integrasjoner/k9sak/K9SakTjeneste.java
@@ -0,0 +1,49 @@
+package no.nav.familie.inntektsmelding.integrasjoner.k9sak;
+
+import java.util.List;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import no.nav.familie.inntektsmelding.integrasjoner.person.PersonIdent;
+import no.nav.familie.inntektsmelding.koder.Ytelsetype;
+import no.nav.familie.inntektsmelding.typer.dto.PeriodeDto;
+import no.nav.familie.inntektsmelding.typer.dto.SaksnummerDto;
+import no.nav.k9.kodeverk.behandling.FagsakYtelseType;
+import no.nav.k9.sak.kontrakt.fagsak.FagsakInfoDto;
+
+@ApplicationScoped
+public class K9SakTjeneste {
+    private K9SakKlient klient;
+
+    public K9SakTjeneste() {
+        // CDI
+    }
+
+    public K9SakTjeneste(K9SakKlient klient) {
+        this.klient = klient;
+    }
+
+    public List<FagsakInfo> hentFagsakInfo(Ytelsetype ytelsetype, PersonIdent personIdent) {
+        List<FagsakInfoDto> k9Fagsaker = klient.hentFagsakInfo(ytelsetype, personIdent.getIdent());
+
+        return k9Fagsaker.stream()
+            .map(k9Fagsak ->
+                new FagsakInfo(
+                    new SaksnummerDto(k9Fagsak.getSaksnummer().getVerdi()),
+                    mapYtelsetype(k9Fagsak.getYtelseType()),
+                    new PeriodeDto(k9Fagsak.getGyldigPeriode().getFom(), k9Fagsak.getGyldigPeriode().getTom())
+                )
+            ).toList();
+    }
+
+    private Ytelsetype mapYtelsetype(FagsakYtelseType fagsagYtelseType) {
+        return switch (fagsagYtelseType) {
+            case PLEIEPENGER_SYKT_BARN -> Ytelsetype.PLEIEPENGER_SYKT_BARN;
+            case PLEIEPENGER_NÆRSTÅENDE -> Ytelsetype.PLEIEPENGER_NÆRSTÅENDE;
+            case OMSORGSPENGER -> Ytelsetype.OMSORGSPENGER;
+            case OPPLÆRINGSPENGER -> Ytelsetype.OPPLÆRINGSPENGER;
+            default -> throw new IllegalArgumentException("Ukjent ytelsetype: " + fagsagYtelseType);
+        };
+    }
+
+}
