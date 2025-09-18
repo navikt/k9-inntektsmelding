af59db35bc8e80b0f1c0149b4f7c121f77250d0c
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2025-09-18 14:26:20 +0200

    Sjekk om det finne sak i k9 i endepunktet hentArbeidsforhold

--- src/main/java/no/nav/familie/inntektsmelding/imdialog/rest/ArbeidsgiverinitiertDialogRest.java
+++ src/main/java/no/nav/familie/inntektsmelding/imdialog/rest/ArbeidsgiverinitiertDialogRest.java
@@ -1,5 +1,7 @@
 package no.nav.familie.inntektsmelding.imdialog.rest;
 
+import java.util.List;
+
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -16,9 +18,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import no.nav.familie.inntektsmelding.imdialog.tjenester.GrunnlagTjeneste;
+import no.nav.familie.inntektsmelding.integrasjoner.k9sak.FagsakInfo;
+import no.nav.familie.inntektsmelding.integrasjoner.k9sak.K9SakTjeneste;
+import no.nav.familie.inntektsmelding.integrasjoner.person.PersonInfo;
+import no.nav.familie.inntektsmelding.integrasjoner.person.PersonTjeneste;
 import no.nav.familie.inntektsmelding.server.auth.api.AutentisertMedTokenX;
 import no.nav.familie.inntektsmelding.server.auth.api.Tilgangskontrollert;
 import no.nav.foreldrepenger.konfig.Environment;
+import no.nav.vedtak.exception.FunksjonellException;
 
 @AutentisertMedTokenX
 @RequestScoped
@@ -33,6 +40,8 @@ public class ArbeidsgiverinitiertDialogRest {
     private static final String HENT_OPPLYSNINGER = "/opplysninger";
 
     private GrunnlagTjeneste grunnlagTjeneste;
+    private PersonTjeneste personTjeneste;
+    private K9SakTjeneste k9SakTjeneste;
     private boolean erProd = true;
 
     ArbeidsgiverinitiertDialogRest() {
@@ -40,8 +49,10 @@ public class ArbeidsgiverinitiertDialogRest {
     }
 
     @Inject
-    public ArbeidsgiverinitiertDialogRest(GrunnlagTjeneste grunnlagTjeneste) {
+    public ArbeidsgiverinitiertDialogRest(GrunnlagTjeneste grunnlagTjeneste, PersonTjeneste personTjeneste, K9SakTjeneste k9SakTjeneste) {
         this.grunnlagTjeneste = grunnlagTjeneste;
+        this.personTjeneste = personTjeneste;
+        this.k9SakTjeneste = k9SakTjeneste;
         this.erProd = Environment.current().isProd();
     }
 
@@ -54,6 +65,21 @@ public class ArbeidsgiverinitiertDialogRest {
             throw new IllegalStateException("Ugyldig kall på restpunkt som ikke er lansert");
         }
         LOG.info("Henter arbeidsforhold for søker");
+
+        // Sjekk at person finnes
+        PersonInfo personInfo = personTjeneste.hentPersonFraIdent(request.fødselsnummer());
+        if (personInfo == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        // Sjekk at søker har sak i k9-sak
+        List<FagsakInfo> fagsakerIK9Sak =  k9SakTjeneste.hentFagsakInfo(request.ytelseType(), request.fødselsnummer());
+        var finnesSakIK9 = fagsakerIK9Sak.stream().anyMatch(fagsak -> fagsak.gyldigPeriode().inneholderDato(request.førsteFraværsdag()));
+        if (!finnesSakIK9) {
+            var feilmelding = String.format("Du kan ikke sende inn inntektsmelding på %s for denne personen", request.ytelseType());
+            throw new FunksjonellException("INGEN_SAK_FUNNET", feilmelding, null, null);
+        }
+
         var response = grunnlagTjeneste.finnArbeidsforholdForFnr(request.fødselsnummer(), request.ytelseType(), request.førsteFraværsdag());
         return response.map(d ->Response.ok(d).build()).orElseGet(() -> Response.status(Response.Status.NOT_FOUND).build());
     }
