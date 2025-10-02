2effb1ca582380262212f3481037b290befa2935
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2025-10-02 15:21:42 +0200

    Set inntektsmelding_type til VARCHAR(50) i stedet for VARCHAR(255)

--- src/main/resources/db/postgres/defaultDS/V1.0_026__add_inntektsmelding_type_column.sql
+++ src/main/resources/db/postgres/defaultDS/V1.0_026__add_inntektsmelding_type_column.sql
@@ -1,5 +1,5 @@
 -- Add inntektsmelding_type column with default value 'ORDINÆR'
-ALTER TABLE INNTEKTSMELDING ADD COLUMN inntektsmelding_type VARCHAR(255) DEFAULT 'ORDINÆR';
+ALTER TABLE INNTEKTSMELDING ADD COLUMN inntektsmelding_type VARCHAR(50) DEFAULT 'ORDINÆR';
 
 -- Make the column NOT NULL (all existing rows will already have 'ORDINÆR' due to default)
 ALTER TABLE INNTEKTSMELDING ALTER COLUMN inntektsmelding_type SET NOT NULL;
