054f38b3d0d9df17f9146c8b268c413005484ac2
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2025-10-15 13:35:44 +0200

    Reapply "TSFF-1931: Generer pdf for refusjonskrav for nyansatte (#527)" (#532)
    This reverts commit 5cc50a104a085439c77fcd551beba02fc91721a1.

--- src/main/java/no/nav/familie/inntektsmelding/integrasjoner/dokgen/InntektsmeldingPdfDataMapper.java
+++ src/main/java/no/nav/familie/inntektsmelding/integrasjoner/dokgen/InntektsmeldingPdfDataMapper.java
@@ -1,15 +1,10 @@
 package no.nav.familie.inntektsmelding.integrasjoner.dokgen;
 
 
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import no.nav.familie.inntektsmelding.imdialog.modell.BortaltNaturalytelseEntitet;
 import no.nav.familie.inntektsmelding.imdialog.modell.InntektsmeldingEntitet;
-import no.nav.familie.inntektsmelding.imdialog.modell.RefusjonsendringEntitet;
 import no.nav.familie.inntektsmelding.integrasjoner.person.PersonInfo;
 import no.nav.familie.inntektsmelding.koder.NaturalytelseType;
 import no.nav.familie.inntektsmelding.utils.FormatUtils;
@@ -46,7 +41,7 @@ public class InntektsmeldingPdfDataMapper {
 
         if (inntektsmelding.getMånedRefusjon() != null) {
             var opphørsdato = inntektsmelding.getOpphørsdatoRefusjon() != null ? inntektsmelding.getOpphørsdatoRefusjon() : null;
-            var refusjonsendringerTilPdf = mapRefusjonsendringPerioder(inntektsmelding.getRefusjonsendringer(), opphørsdato, inntektsmelding.getMånedRefusjon(), startdato);
+            var refusjonsendringerTilPdf = PdfDataMapperUtil.mapRefusjonsendringPerioder(inntektsmelding.getRefusjonsendringer(), opphørsdato, inntektsmelding.getMånedRefusjon(), startdato);
             imDokumentdataBuilder.medRefusjonsendringer(refusjonsendringerTilPdf);
             imDokumentdataBuilder.medAntallRefusjonsperioder(refusjonsendringerTilPdf.size());
         } else {
@@ -99,28 +94,5 @@ public class InntektsmeldingPdfDataMapper {
             case YRKEBIL_TJENESTLIGBEHOV_LISTEPRIS -> "Yrkesbil tjenesteligbehov listepris";
             case INNBETALING_TIL_UTENLANDSK_PENSJONSORDNING -> "Innbetaling utenlandsk pensjonsordning";
         };
-    }
-
-    private static List<RefusjonsendringPeriode> mapRefusjonsendringPerioder(List<RefusjonsendringEntitet> refusjonsendringer,
-                                                                             LocalDate opphørsdato,
-                                                                             BigDecimal refusjonsbeløp,
-                                                                             LocalDate startdato) {
-        List<RefusjonsendringPeriode> refusjonsendringerTilBrev = new ArrayList<>();
-
-        //første perioden
-        refusjonsendringerTilBrev.add(new RefusjonsendringPeriode(FormatUtils.formaterDatoForLister(startdato), startdato, refusjonsbeløp));
-
-        refusjonsendringerTilBrev.addAll(
-            refusjonsendringer.stream().map(rpe -> new RefusjonsendringPeriode(FormatUtils.formaterDatoForLister(rpe.getFom()), rpe.getFom(), rpe.getRefusjonPrMnd()))
-            .toList());
-
-        if (opphørsdato != null && !opphørsdato.equals(Tid.TIDENES_ENDE)) {
-            // Da opphørsdato er siste dag med refusjon må vi legge til denne mappingen for å få det rett ut i PDF, da vi ønsker å vise når første dag uten refusjon er
-            refusjonsendringerTilBrev.add(new RefusjonsendringPeriode(FormatUtils.formaterDatoForLister(opphørsdato.plusDays(1)), opphørsdato.plusDays(1), BigDecimal.ZERO));
-        }
-
-        return refusjonsendringerTilBrev.stream()
-            .sorted(Comparator.comparing(RefusjonsendringPeriode::fraDato))
-            .toList();
     }
 }
