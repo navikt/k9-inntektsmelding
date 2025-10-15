7e6ac7be12408abcea910006dfa0bc2e8cf9ba9b
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2025-10-15 13:55:13 +0200

    Reapply TSFF-1931: Generer pdf for refusjonskrav for nyansatte (#534)
    ### **Behov / Bakgrunn**
    Generering av den nye PDFen feilet.
    
    ### **Løsning**
    - Reapply "TSFF-1931: Generer pdf for refusjonskrav for nyansatte (#527)" (#532)
    - Må legge til avsenderSystem

--- src/main/java/no/nav/familie/inntektsmelding/utils/mapper/PdfDataMapperUtil.java
+++ src/main/java/no/nav/familie/inntektsmelding/utils/mapper/PdfDataMapperUtil.java
@@ -1,13 +1,21 @@
 package no.nav.familie.inntektsmelding.utils.mapper;
 
-import no.nav.familie.inntektsmelding.imdialog.modell.EndringsårsakEntitet;
-import no.nav.familie.inntektsmelding.imdialog.modell.InntektsmeldingEntitet;
-import no.nav.familie.inntektsmelding.integrasjoner.dokgen.Endringsarsak;
-import no.nav.familie.inntektsmelding.integrasjoner.dokgen.Kontaktperson;
+import static no.nav.familie.inntektsmelding.utils.FormatUtils.formaterDatoForLister;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
-import static no.nav.familie.inntektsmelding.utils.FormatUtils.formaterDatoForLister;
+import no.nav.familie.inntektsmelding.imdialog.modell.EndringsårsakEntitet;
+import no.nav.familie.inntektsmelding.imdialog.modell.InntektsmeldingEntitet;
+import no.nav.familie.inntektsmelding.imdialog.modell.RefusjonsendringEntitet;
+import no.nav.familie.inntektsmelding.integrasjoner.dokgen.Endringsarsak;
+import no.nav.familie.inntektsmelding.integrasjoner.dokgen.Kontaktperson;
+import no.nav.familie.inntektsmelding.integrasjoner.dokgen.RefusjonsendringPeriode;
+import no.nav.familie.inntektsmelding.utils.FormatUtils;
+import no.nav.vedtak.konfig.Tid;
 
 public final class PdfDataMapperUtil {
 
@@ -29,5 +37,28 @@ public final class PdfDataMapperUtil {
         return new Kontaktperson(
             inntektsmelding.getKontaktperson().getNavn(),
             inntektsmelding.getKontaktperson().getTelefonnummer());
+    }
+
+    public static List<RefusjonsendringPeriode> mapRefusjonsendringPerioder(List<RefusjonsendringEntitet> refusjonsendringer,
+                                                                             LocalDate opphørsdato,
+                                                                             BigDecimal refusjonsbeløp,
+                                                                             LocalDate startdato) {
+        List<RefusjonsendringPeriode> refusjonsendringerTilBrev = new ArrayList<>();
+
+        //første perioden
+        refusjonsendringerTilBrev.add(new RefusjonsendringPeriode(FormatUtils.formaterDatoForLister(startdato), startdato, refusjonsbeløp));
+
+        refusjonsendringerTilBrev.addAll(
+            refusjonsendringer.stream().map(rpe -> new RefusjonsendringPeriode(FormatUtils.formaterDatoForLister(rpe.getFom()), rpe.getFom(), rpe.getRefusjonPrMnd()))
+                .toList());
+
+        if (opphørsdato != null && !opphørsdato.equals(Tid.TIDENES_ENDE)) {
+            // Da opphørsdato er siste dag med refusjon må vi legge til denne mappingen for å få det rett ut i PDF, da vi ønsker å vise når første dag uten refusjon er
+            refusjonsendringerTilBrev.add(new RefusjonsendringPeriode(FormatUtils.formaterDatoForLister(opphørsdato.plusDays(1)), opphørsdato.plusDays(1), BigDecimal.ZERO));
+        }
+
+        return refusjonsendringerTilBrev.stream()
+            .sorted(Comparator.comparing(RefusjonsendringPeriode::fraDato))
+            .toList();
     }
 }
