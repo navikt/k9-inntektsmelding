6acb6cbded4dbff1c014394e56518416cfa57fd3
Qadeer Khan <ahm.qadeer@gmail.com>
2026-01-12 10:09:37 +0100

    støtte for bigquery metrikker og prosesstask (#581)
    ### **Behov / Bakgrunn**
    Felles prosesstask oversikt viser ikke k9-inntektsmelding tasker
    
    ### **Løsning**
    Legger inn støtte for feilede prosesstasker mot BigQuery

--- src/main/java/no/nav/familie/inntektsmelding/prosesstask/ProsessTaskRepository.java
+++ src/main/java/no/nav/familie/inntektsmelding/prosesstask/ProsessTaskRepository.java
@@ -2,10 +2,13 @@ package no.nav.familie.inntektsmelding.prosesstask;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
+
+import no.nav.vedtak.felles.prosesstask.impl.ProsessTaskEntitet;
 
 @ApplicationScoped
 public class ProsessTaskRepository {
@@ -20,18 +23,18 @@ public class ProsessTaskRepository {
         this.entityManager = entityManager;
     }
 
-    public long tellAntallFeilendeProsessTasker() {
+    public List<ProsessTaskEntitet> feiledeProsessTasker() {
         return entityManager.createQuery("""
-                SELECT COUNT(p)
+                SELECT p
                 FROM ProsessTaskEntitet p
                 WHERE (
                     p.status = 'FEILET'
                     OR ((p.status IN ('KLAR', 'VETO')) AND p.opprettetTid < :tidspunkt_i_dag_tidlig AND (p.nesteKjøringEtter IS NULL OR p.nesteKjøringEtter < :tidspunkt_nå))
                     OR (p.status IN ('VENTER_SVAR', 'SUSPENDERT') AND p.opprettetTid < :tidspunkt_i_dag_tidlig)
                 )
-                """, Long.class)
+                """, ProsessTaskEntitet.class)
             .setParameter("tidspunkt_i_dag_tidlig", LocalDateTime.now().truncatedTo(ChronoUnit.DAYS))
             .setParameter("tidspunkt_nå", LocalDateTime.now())
-            .getSingleResult();
+            .getResultList();
     }
 }
