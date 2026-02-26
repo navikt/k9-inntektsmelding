f87ab5a0afbc252183edae902e146c0c65600fac
oyvemb <107673448+oyvemb@users.noreply.github.com>
2026-02-26 09:11:10 +0100

    TSFF-2427: Autorisering med altinn 3 (#622)
    ### **Behov / Bakgrunn**
    https://jira.adeo.no/browse/TSFF-2427
    
    ### **Løsning**
    Kopierer
    https://github.com/navikt/fp-inntektsmelding/blob/master/src/main/java/no/nav/familie/inntektsmelding/integrasjoner/altinn/ArbeidsgiverAltinnTilgangerKlient.java
    Lager i første omgang en løsning for å logge resultatet av nytt kall
    uten at det påvirker tilgangsbeslutningen. Neste steg blir å ta i bruk
    den nye klienten med altinn 2, og så med altinn 3.

---
+++ src/main/java/no/nav/familie/inntektsmelding/integrasjoner/altinn/ArbeidsgiverAltinnTilgangerKlient.java
@@ -0,0 +1,119 @@
+package no.nav.familie.inntektsmelding.integrasjoner.altinn;
+
+import java.util.List;
+import java.util.Map;
+
+import jakarta.ws.rs.core.UriBuilder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import no.nav.foreldrepenger.konfig.Environment;
+import no.nav.vedtak.exception.IntegrasjonException;
+import no.nav.vedtak.felles.integrasjon.rest.NavHeaders;
+import no.nav.vedtak.felles.integrasjon.rest.RestClient;
+import no.nav.vedtak.felles.integrasjon.rest.RestClientConfig;
+import no.nav.vedtak.felles.integrasjon.rest.RestConfig;
+import no.nav.vedtak.felles.integrasjon.rest.RestRequest;
+import no.nav.vedtak.felles.integrasjon.rest.TokenFlow;
+
+// Denne klienten opererer med TokenX derfor trenger man en resource.
+@RestClientConfig(tokenConfig = TokenFlow.ADAPTIVE, endpointProperty = "arbeidsgiver.altinn.tilganger.url", scopesProperty = "arbeidsgiver.altinn.tilganger.resource")
+public class ArbeidsgiverAltinnTilgangerKlient {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ArbeidsgiverAltinnTilgangerKlient.class);
+
+    private static final Environment ENV = Environment.current();
+
+    public static final String ALTINN_TO_TJENESTE = "4936:1";
+    public static final String ALTINN_TRE_RESSURS = ENV.getRequiredProperty("altinn.tre.inntektsmelding.ressurs");
+
+    public static final boolean BRUK_ALTINN_TRE_FOR_TILGANGSKONTROLL = ENV.getProperty("bruk.altinn.tre.for.tilgangskontroll.toggle", boolean.class, false);
+
+    private static final String ALTINN_TILGANGER_PATH = "/altinn-tilganger";
+
+    private static ArbeidsgiverAltinnTilgangerKlient instance = new ArbeidsgiverAltinnTilgangerKlient();
+
+    private final RestClient restClient;
+    private final RestConfig restConfig;
+
+    private ArbeidsgiverAltinnTilgangerKlient() {
+        this(RestClient.client());
+    }
+
+    ArbeidsgiverAltinnTilgangerKlient(RestClient restClient) {
+        this.restClient = restClient;
+        this.restConfig = RestConfig.forClient(this.getClass());
+    }
+
+    public static synchronized ArbeidsgiverAltinnTilgangerKlient instance() {
+        var inst = instance;
+        if (inst == null) {
+            inst = new ArbeidsgiverAltinnTilgangerKlient();
+            instance = inst;
+        }
+        return inst;
+    }
+
+    // TODO: Må ryddes opp etter Altinn 3 ressurs overgang i prod.
+    public boolean harTilgangTilBedriften(String orgnr) {
+        Map<String, List<String>> altinnRessurserBrukerHarTilgangTilPerOrgnr = hentTilganger().orgNrTilTilganger();
+
+        if (altinnRessurserBrukerHarTilgangTilPerOrgnr == null || altinnRessurserBrukerHarTilgangTilPerOrgnr.isEmpty()
+            || !altinnRessurserBrukerHarTilgangTilPerOrgnr.containsKey(orgnr)) {
+            return false;
+        }
+
+        List<String> brukersTilgangerForOrgnr = altinnRessurserBrukerHarTilgangTilPerOrgnr.get(orgnr);
+
+        boolean tilgangsbeslutningAltinn2 = brukersTilgangerForOrgnr.contains(ALTINN_TO_TJENESTE);
+        boolean tilgangsbeslutningAltinn3 = brukersTilgangerForOrgnr.contains(ALTINN_TRE_RESSURS);
+
+        if (tilgangsbeslutningAltinn2 != tilgangsbeslutningAltinn3) { // hvis tilgang er ulikt mellom Altinn 2 og Altinn 3, logg for avstemming.
+            LOG.warn("ALTINN: Tilgangsbeslutninger er ulike for bruker! Altinn 2: {}, Altinn 3: {}.", tilgangsbeslutningAltinn2, tilgangsbeslutningAltinn3);
+        }
+
+        return BRUK_ALTINN_TRE_FOR_TILGANGSKONTROLL ? tilgangsbeslutningAltinn3 : tilgangsbeslutningAltinn2;
+    }
+
+    private ArbeidsgiverAltinnTilgangerResponse hentTilganger() {
+        var uri = UriBuilder.fromUri(restConfig.endpoint()).path(ALTINN_TILGANGER_PATH).build();
+        var request = RestRequest.newPOSTJson(lagRequestFilter(), uri, restConfig);
+        request.otherCallId(NavHeaders.HEADER_NAV_CORRELATION_ID);
+        try {
+            return restClient.send(request, ArbeidsgiverAltinnTilgangerResponse.class);
+        } catch (RuntimeException e) {
+            throw new IntegrasjonException("K9-965432",
+                "Feil ved kall til arbeidsgiver-altinn-tilganger. Meld til #team_fager hvis dette skjer over lengre tidsperiode.", e);
+        }
+    }
+
+    private ArbeidsgiverAltinnTilgangerRequest lagRequestFilter() {
+        return new ArbeidsgiverAltinnTilgangerRequest(new ArbeidsgiverAltinnTilgangerRequest.FilterCriteria(
+            List.of(ALTINN_TO_TJENESTE),
+            List.of(ALTINN_TRE_RESSURS)));
+    }
+
+    public record ArbeidsgiverAltinnTilgangerRequest(FilterCriteria filter) {
+        public record FilterCriteria(@JsonProperty("altinn2Tilganger") List<String> altinn2Tilganger,
+                                     @JsonProperty("altinn3Tilganger") List<String> altinn3Tilganger) {
+        }
+    }
+
+    public record ArbeidsgiverAltinnTilgangerResponse(boolean isError,
+                                                      List<Organisasjon> hierarki,
+                                                      Map<String, List<String>> orgNrTilTilganger,
+                                                      Map<String, List<String>> tilgangTilOrgNr) {
+
+        public record Organisasjon(String orgnr,
+                                   @JsonProperty("altinn3Tilganger") List<String> altinn3Tilganger,
+                                   @JsonProperty("altinn2Tilganger") List<String> altinn2Tilganger,
+                                   List<Organisasjon> underenheter,
+                                   String navn,
+                                   String organisasjonsform,
+                                   boolean erSlettet) {
+        }
+    }
+}
---
+++ src/test/java/no/nav/familie/inntektsmelding/integrasjoner/altinn/Altinn3ArbeidsgiverAltinnTilgangerKlientTest.java
@@ -0,0 +1,95 @@
+package no.nav.familie.inntektsmelding.integrasjoner.altinn;
+
+import static no.nav.familie.inntektsmelding.integrasjoner.altinn.ArbeidsgiverAltinnTilgangerKlient.ALTINN_TO_TJENESTE;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import no.nav.vedtak.felles.integrasjon.rest.RestClient;
+import no.nav.vedtak.felles.integrasjon.rest.RestRequest;
+import no.nav.vedtak.sikkerhet.kontekst.BasisKontekst;
+import no.nav.vedtak.sikkerhet.kontekst.KontekstHolder;
+
+@Disabled
+@ExtendWith(MockitoExtension.class)
+class Altinn3ArbeidsgiverAltinnTilgangerKlientTest {
+
+    protected static final String NAV_TEST_RESSURS = "nav_test_ressurs";
+    protected static final String TEST_ORGNR = "999999999";
+    protected static final String BRUK_ALTINN_TRE_FOR_TILGANGSKONTROLL_TOGGLE = "bruk.altinn.tre.for.tilgangskontroll.toggle";
+
+    @Mock
+    RestClient klient;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.setProperty(BRUK_ALTINN_TRE_FOR_TILGANGSKONTROLL_TOGGLE, "true");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.clearProperty(BRUK_ALTINN_TRE_FOR_TILGANGSKONTROLL_TOGGLE);
+    }
+
+    @BeforeEach
+    void setUp() {
+        KontekstHolder.setKontekst(BasisKontekst.ikkeAutentisertRequest("fp-inntektsmelding"));
+        System.setProperty("altinn.tre.inntektsmelding.ressurs", NAV_TEST_RESSURS);
+    }
+
+    @AfterEach
+    void tearDown() {
+        KontekstHolder.fjernKontekst();
+        System.clearProperty("altinn.tre.inntektsmelding.ressurs");
+    }
+
+    @Test
+    void sjekkTilgang__har_tilgang_til_en_bedrift_altinn_3_tjeneste_ok() {
+        var altinnAutoriseringKlient = new ArbeidsgiverAltinnTilgangerKlient(klient);
+        when(klient.send(any(RestRequest.class), any())).thenReturn(lagOrgNrTilTilgangResponse(TEST_ORGNR, NAV_TEST_RESSURS));
+        assertThat(altinnAutoriseringKlient.harTilgangTilBedriften(TEST_ORGNR)).isTrue();
+        verify(klient).send(any(RestRequest.class), any());
+    }
+
+    @Test
+    void sjekkTilgang__har_tilgang_til_en_bedrift_altinn_3_tjeneste_nok() {
+        var altinnAutoriseringKlient = new ArbeidsgiverAltinnTilgangerKlient(klient);
+        when(klient.send(any(RestRequest.class), any())).thenReturn(lagOrgNrTilTilgangResponse(TEST_ORGNR, NAV_TEST_RESSURS));
+        assertThat(altinnAutoriseringKlient.harTilgangTilBedriften("000000000")).isFalse();
+        verify(klient).send(any(RestRequest.class), any());
+    }
+
+    @Test
+    void sjekkTilgang__har_tilgang_til_en_bedrift_altinn_3_ikke_har_tilgang_til_tjeneste_nok() {
+        var altinnAutoriseringKlient = new ArbeidsgiverAltinnTilgangerKlient(klient);
+        when(klient.send(any(RestRequest.class), any())).thenReturn(lagOrgNrTilTilgangResponse(TEST_ORGNR, ALTINN_TO_TJENESTE));
+        assertThat(altinnAutoriseringKlient.harTilgangTilBedriften(TEST_ORGNR)).isFalse();
+        verify(klient).send(any(RestRequest.class), any());
+    }
+
+    @Test
+    void sjekkTilgang__har_tilgang_til_en_bedrift_som_ikke_har_tilgang_til_altinn_3_tjeneste_nok() {
+        var altinnAutoriseringKlient = new ArbeidsgiverAltinnTilgangerKlient(klient);
+        when(klient.send(any(RestRequest.class), any())).thenReturn(lagOrgNrTilTilgangResponse(TEST_ORGNR, ALTINN_TO_TJENESTE));
+        assertThat(altinnAutoriseringKlient.harTilgangTilBedriften("000000000")).isFalse();
+        verify(klient).send(any(RestRequest.class), any());
+    }
+
+    private ArbeidsgiverAltinnTilgangerKlient.ArbeidsgiverAltinnTilgangerResponse lagOrgNrTilTilgangResponse(String orgnr, String... tilganger) {
+        return new ArbeidsgiverAltinnTilgangerKlient.ArbeidsgiverAltinnTilgangerResponse(false, List.of(), Map.of(orgnr, List.of(tilganger)), null);
+    }
+}
