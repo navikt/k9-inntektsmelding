f4d51d40507ed28329e86a63dd08e6358471a396
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2026-02-27 13:20:53 +0100

    TSFF-2427: Fjerner gammel klient

--- src/main/java/no/nav/familie/inntektsmelding/pip/AltinnTilgangTjeneste.java
+++ src/main/java/no/nav/familie/inntektsmelding/pip/AltinnTilgangTjeneste.java
@@ -1,42 +1,24 @@
 package no.nav.familie.inntektsmelding.pip;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import jakarta.enterprise.context.Dependent;
 
-import no.nav.familie.inntektsmelding.integrasjoner.altinn.AltinnAutoriseringKlient;
 import no.nav.familie.inntektsmelding.integrasjoner.altinn.ArbeidsgiverAltinnTilgangerKlient;
 
 @Dependent
 public class AltinnTilgangTjeneste {
 
-    private static final Logger LOG = LoggerFactory.getLogger(AltinnTilgangTjeneste.class);
-
-    private final AltinnAutoriseringKlient altinnKlient;
-    private final ArbeidsgiverAltinnTilgangerKlient nyAltinnKlient;
+    private final ArbeidsgiverAltinnTilgangerKlient altinnKlient;
 
     public AltinnTilgangTjeneste() {
-        this(AltinnAutoriseringKlient.instance(), ArbeidsgiverAltinnTilgangerKlient.instance());
+        this(ArbeidsgiverAltinnTilgangerKlient.instance());
     }
 
-    public AltinnTilgangTjeneste(AltinnAutoriseringKlient altinnKlient, ArbeidsgiverAltinnTilgangerKlient nyAltinnKlient) {
+    public AltinnTilgangTjeneste(ArbeidsgiverAltinnTilgangerKlient altinnKlient) {
         this.altinnKlient = altinnKlient;
-        this.nyAltinnKlient = nyAltinnKlient;
     }
 
     public boolean harTilgangTilBedriften(String orgNr) {
-        boolean gammelHarTilgang = altinnKlient.harTilgangTilBedriften(orgNr);
-        try {
-            boolean nyHarTilgang = nyAltinnKlient.harTilgangTilBedriften(orgNr);
-            if (nyHarTilgang != gammelHarTilgang) {
-                LOG.warn("ALTINN: gammelHarTilgang: {}, nyHarTilgang: {}", gammelHarTilgang, nyHarTilgang);
-            }
-            return nyHarTilgang;
-        } catch (Exception e) {
-            LOG.warn("Kall på ny altinnklient feilet", e);
-        }
-        return gammelHarTilgang;
+        return altinnKlient.harTilgangTilBedriften(orgNr);
     }
 
     public boolean manglerTilgangTilBedriften(String orgNr) {
