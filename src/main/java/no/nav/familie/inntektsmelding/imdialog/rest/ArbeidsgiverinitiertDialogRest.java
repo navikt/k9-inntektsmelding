a7e5afab4f99e4f102811496b1c4cb69bfc44bbd
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2026-03-03 08:38:22 +0100

    Flytt validering til egen tjeneste

--- src/main/java/no/nav/familie/inntektsmelding/imdialog/rest/ArbeidsgiverinitiertDialogRest.java
+++ src/main/java/no/nav/familie/inntektsmelding/imdialog/rest/ArbeidsgiverinitiertDialogRest.java
@@ -1,8 +1,5 @@
 package no.nav.familie.inntektsmelding.imdialog.rest;
 
-import java.time.LocalDate;
-import java.util.List;
-
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
@@ -18,9 +15,8 @@ import jakarta.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import no.nav.familie.inntektsmelding.imdialog.tjenester.ArbeidsgiverinitiertDialogRestValiderer;
 import no.nav.familie.inntektsmelding.imdialog.tjenester.GrunnlagTjeneste;
-import no.nav.familie.inntektsmelding.integrasjoner.k9sak.FagsakInfo;
-import no.nav.familie.inntektsmelding.integrasjoner.k9sak.K9SakTjeneste;
 import no.nav.familie.inntektsmelding.integrasjoner.person.PersonInfo;
 import no.nav.familie.inntektsmelding.integrasjoner.person.PersonTjeneste;
 import no.nav.familie.inntektsmelding.koder.ForespørselType;
@@ -28,10 +24,6 @@ import no.nav.familie.inntektsmelding.koder.Ytelsetype;
 import no.nav.familie.inntektsmelding.server.auth.api.AutentisertMedTokenX;
 import no.nav.familie.inntektsmelding.server.auth.api.Tilgangskontrollert;
 import no.nav.familie.inntektsmelding.typer.dto.KodeverkMapper;
-import no.nav.familie.inntektsmelding.typer.dto.PeriodeDto;
-import no.nav.familie.inntektsmelding.typer.dto.YtelseTypeDto;
-import no.nav.k9.sak.typer.AktørId;
-import no.nav.vedtak.exception.FunksjonellException;
 
 @AutentisertMedTokenX
 @RequestScoped
@@ -47,20 +39,19 @@ public class ArbeidsgiverinitiertDialogRest {
     private static final String HENT_ARBEIDSGIVERE_UREGISTRERT = "/arbeidsgivere/uregistrert";
     private static final String HENT_OPPLYSNINGER_UREGISTRERT = "/opplysninger/uregistrert";
 
-
     private GrunnlagTjeneste grunnlagTjeneste;
     private PersonTjeneste personTjeneste;
-    private K9SakTjeneste k9SakTjeneste;
+    private ArbeidsgiverinitiertDialogRestValiderer arbeidsgiverinitiertDialogRestValiderer;
 
     ArbeidsgiverinitiertDialogRest() {
         // CDI
     }
 
     @Inject
-    public ArbeidsgiverinitiertDialogRest(GrunnlagTjeneste grunnlagTjeneste, PersonTjeneste personTjeneste, K9SakTjeneste k9SakTjeneste) {
+    public ArbeidsgiverinitiertDialogRest(GrunnlagTjeneste grunnlagTjeneste, PersonTjeneste personTjeneste, ArbeidsgiverinitiertDialogRestValiderer arbeidsgiverinitiertDialogRestValiderer) {
         this.grunnlagTjeneste = grunnlagTjeneste;
         this.personTjeneste = personTjeneste;
-        this.k9SakTjeneste = k9SakTjeneste;
+        this.arbeidsgiverinitiertDialogRestValiderer = arbeidsgiverinitiertDialogRestValiderer;
     }
 
     @POST
@@ -76,7 +67,7 @@ public class ArbeidsgiverinitiertDialogRest {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-        validerSakIK9(personInfo, request.ytelseType(), request.førsteFraværsdag());
+        arbeidsgiverinitiertDialogRestValiderer.validerSakIK9(personInfo, request.ytelseType(), request.førsteFraværsdag());
 
         var response = grunnlagTjeneste.finnArbeidsforholdForFnr(request.fødselsnummer(), request.førsteFraværsdag());
         return response.map(d ->Response.ok(d).build()).orElseGet(() -> Response.status(Response.Status.NOT_FOUND).build());
@@ -119,44 +110,11 @@ public class ArbeidsgiverinitiertDialogRest {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
-        validerSakIK9(personInfo, request.ytelseType(), request.førsteFraværsdag());
-        validerAtOrgnummerIkkeFinnesIAaregPåPerson(request, personInfo);
+        arbeidsgiverinitiertDialogRestValiderer.validerSakIK9(personInfo, request.ytelseType(), request.førsteFraværsdag());
+        arbeidsgiverinitiertDialogRestValiderer.validerAtOrgnummerIkkeFinnesIAaregPåPerson(request, personInfo);
 
         Ytelsetype ytelsetype = KodeverkMapper.mapYtelsetype(request.ytelseType());
         var hentOpplysningerResponse = grunnlagTjeneste.hentOpplysninger(request.fødselsnummer(), ytelsetype, request.førsteFraværsdag(), request.organisasjonsnummer(), ForespørselType.ARBEIDSGIVERINITIERT_UREGISTRERT);
         return Response.ok(hentOpplysningerResponse).build();
-    }
-
-    // TODO: burde denne trekkes ut til grunnlag-tjenesten?
-    private void validerSakIK9(PersonInfo personInfo, YtelseTypeDto ytelseType, LocalDate førsteFraværsdag) {
-        // Sjekk at søker har sak i k9-sak
-        Ytelsetype ytelsetype = KodeverkMapper.mapYtelsetype(ytelseType);
-        AktørId aktørId = new AktørId(personInfo.aktørId().getAktørId());
-        List<FagsakInfo> fagsakerIK9Sak =  k9SakTjeneste.hentFagsakInfo(ytelsetype, aktørId);
-        List<PeriodeDto> søknadsPerioderForFagsakerIK9 = fagsakerIK9Sak.stream()
-            .flatMap(fagsak -> fagsak.søknadsPerioder().stream())
-            .toList();
-
-        var finnesSakIK9 = søknadsPerioderForFagsakerIK9.stream()
-            .anyMatch(søknandsperiode -> søknandsperiode.inneholderDato(førsteFraværsdag));
-
-        if (!finnesSakIK9) {
-            var feilmelding = String.format("Du kan ikke sende inn inntektsmelding på %s for denne personen", ytelsetype);
-            throw new FunksjonellException("INGEN_SAK_FUNNET", feilmelding, null, null);
-        }
-
-        if (fagsakerIK9Sak.stream().anyMatch(FagsakInfo::venterForTidligSøknad)) {
-            var feilmelding = String.format("Du kan ikke sende inn inntektsmelding før fire uker før denne personen starter med %s", ytelsetype);
-            throw new FunksjonellException("SENDT_FOR_TIDLIG", feilmelding, null, null);
-        }
-    }
-
-    //Er denne sjekken i det hele tatt er nødvendig?
-    private void validerAtOrgnummerIkkeFinnesIAaregPåPerson(OpplysningerRequestDto request, PersonInfo personInfo) {
-        var finnesOrgnummerIAaReg = grunnlagTjeneste.finnesOrgnummerIAaregPåPerson(personInfo.fødselsnummer(), request.organisasjonsnummer().orgnr(), request.førsteFraværsdag());
-        if (finnesOrgnummerIAaReg) {
-            var tekst = "Det finnes rapportering i aa-registeret på organisasjonsnummeret. Nav vil be om inntektsmelding når vi trenger det";
-            throw new FunksjonellException("FINNES_I_AAREG", tekst, null, null);
-        }
     }
 }
---
+++ src/main/java/no/nav/familie/inntektsmelding/imdialog/tjenester/ArbeidsgiverinitiertDialogRestValiderer.java
@@ -0,0 +1,63 @@
+package no.nav.familie.inntektsmelding.imdialog.tjenester;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
+
+import no.nav.familie.inntektsmelding.imdialog.rest.OpplysningerRequestDto;
+import no.nav.familie.inntektsmelding.integrasjoner.k9sak.FagsakInfo;
+import no.nav.familie.inntektsmelding.integrasjoner.k9sak.K9SakTjeneste;
+import no.nav.familie.inntektsmelding.integrasjoner.person.PersonInfo;
+import no.nav.familie.inntektsmelding.koder.Ytelsetype;
+import no.nav.familie.inntektsmelding.typer.dto.KodeverkMapper;
+import no.nav.familie.inntektsmelding.typer.dto.PeriodeDto;
+import no.nav.familie.inntektsmelding.typer.dto.YtelseTypeDto;
+import no.nav.k9.sak.typer.AktørId;
+import no.nav.vedtak.exception.FunksjonellException;
+
+@Dependent
+public class ArbeidsgiverinitiertDialogRestValiderer {
+
+    private GrunnlagTjeneste grunnlagTjeneste;
+    private K9SakTjeneste k9SakTjeneste;
+
+
+    @Inject
+    public ArbeidsgiverinitiertDialogRestValiderer(GrunnlagTjeneste grunnlagTjeneste, K9SakTjeneste k9SakTjeneste) {
+        this.grunnlagTjeneste = grunnlagTjeneste;
+        this.k9SakTjeneste = k9SakTjeneste;
+    }
+
+    public void validerSakIK9(PersonInfo personInfo, YtelseTypeDto ytelseType, LocalDate førsteFraværsdag) {
+        // Sjekk at søker har sak i k9-sak
+        Ytelsetype ytelsetype = KodeverkMapper.mapYtelsetype(ytelseType);
+        AktørId aktørId = new AktørId(personInfo.aktørId().getAktørId());
+        List<FagsakInfo> fagsakerIK9Sak =  k9SakTjeneste.hentFagsakInfo(ytelsetype, aktørId);
+        List<PeriodeDto> søknadsPerioderForFagsakerIK9 = fagsakerIK9Sak.stream()
+            .flatMap(fagsak -> fagsak.søknadsPerioder().stream())
+            .toList();
+
+        var finnesSakIK9 = søknadsPerioderForFagsakerIK9.stream()
+            .anyMatch(søknandsperiode -> søknandsperiode.inneholderDato(førsteFraværsdag));
+
+        if (!finnesSakIK9) {
+            var feilmelding = String.format("Du kan ikke sende inn inntektsmelding på %s for denne personen", ytelsetype);
+            throw new FunksjonellException("INGEN_SAK_FUNNET", feilmelding, null, null);
+        }
+
+        if (fagsakerIK9Sak.stream().anyMatch(FagsakInfo::venterForTidligSøknad)) {
+            var feilmelding = String.format("Du kan ikke sende inn inntektsmelding før fire uker før denne personen starter med %s", ytelsetype);
+            throw new FunksjonellException("SENDT_FOR_TIDLIG", feilmelding, null, null);
+        }
+    }
+
+    public void validerAtOrgnummerIkkeFinnesIAaregPåPerson(OpplysningerRequestDto request, PersonInfo personInfo) {
+        var finnesOrgnummerIAaReg = grunnlagTjeneste.finnesOrgnummerIAaregPåPerson(personInfo.fødselsnummer(), request.organisasjonsnummer().orgnr(), request.førsteFraværsdag());
+        if (finnesOrgnummerIAaReg) {
+            var tekst = "Det finnes rapportering i aa-registeret på organisasjonsnummeret. Nav vil be om inntektsmelding når vi trenger det";
+            throw new FunksjonellException("FINNES_I_AAREG", tekst, null, null);
+        }
+    }
+}
