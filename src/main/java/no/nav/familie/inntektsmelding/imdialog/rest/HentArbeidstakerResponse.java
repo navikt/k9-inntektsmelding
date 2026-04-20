4482335cb7824246a8e3a171fb624f66988710f0
vebnor <vebjorn.nordby@nav.no>
2026-04-20 15:48:03 +0200

    Utvider HentArbeidstakerResponse med AktørId

--- src/main/java/no/nav/familie/inntektsmelding/imdialog/rest/HentArbeidstakerResponse.java
+++ src/main/java/no/nav/familie/inntektsmelding/imdialog/rest/HentArbeidstakerResponse.java
@@ -3,10 +3,13 @@ package no.nav.familie.inntektsmelding.imdialog.rest;
 import jakarta.validation.constraints.NotNull;
 
 import no.nav.familie.inntektsmelding.typer.dto.Kjønn;
+import no.nav.familie.inntektsmelding.typer.entitet.AktørIdEntitet;
 
 public record HentArbeidstakerResponse(@NotNull String fornavn,
                                        String mellomnavn,
                                        @NotNull String etternavn,
-                                       @NotNull Kjønn kjønn) {
+                                       @NotNull Kjønn kjønn,
+                                       @NotNull AktørIdEntitet aktørId
+) {
 }
 
