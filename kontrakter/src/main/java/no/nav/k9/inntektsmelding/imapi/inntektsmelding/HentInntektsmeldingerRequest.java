0e97cfe1877f2969142ccd629bb12de1c78a93c5
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2026-06-01 13:39:32 +0200

    Rename InntektsmeldingFilterRequest -> HentInntektsmeldingerRequest og fjern unødvending @Valid

--- kontrakter/src/main/java/no/nav/k9/inntektsmelding/imapi/inntektsmelding/HentInntektsmeldingerRequest.java
+++ kontrakter/src/main/java/no/nav/k9/inntektsmelding/imapi/inntektsmelding/HentInntektsmeldingerRequest.java
@@ -10,10 +10,10 @@ import no.nav.k9.inntektsmelding.felles.FødselsnummerDto;
 import no.nav.k9.inntektsmelding.felles.OrganisasjonsnummerDto;
 import no.nav.k9.inntektsmelding.felles.YtelseTypeDto;
 
-public record InntektsmeldingFilterRequest(@NotNull @Valid OrganisasjonsnummerDto orgnr,
+public record HentInntektsmeldingerRequest(@NotNull @Valid OrganisasjonsnummerDto orgnr,
                                            @Valid FødselsnummerDto fnr,
                                            @Valid YtelseTypeDto ytelseType,
                                            @Valid UUID forespørselUuid,
-                                           @Valid LocalDate fom,
-                                           @Valid LocalDate tom) {
+                                           LocalDate fom,
+                                           LocalDate tom) {
 }
