7a2fc531e128e45bcb1329b476e95c60d4d21251
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2026-06-01 14:59:06 +0200

    FødselsnummerDto sin toString skal ikke returnere noe

--- kontrakter/src/main/java/no/nav/k9/inntektsmelding/felles/FødselsnummerDto.java
+++ kontrakter/src/main/java/no/nav/k9/inntektsmelding/felles/FødselsnummerDto.java
@@ -8,17 +8,6 @@ import com.fasterxml.jackson.annotation.JsonValue;
 public record FødselsnummerDto(@JsonValue @NotNull @Pattern(regexp = "^\\d{11}$") @NotNull String fnr) {
     @Override
     public String toString() {
-        return getClass().getSimpleName() + "<" + masker() + ">";
-    }
-
-    private String masker() {
-        if (fnr == null) {
-            return "";
-        }
-        var length = fnr.length();
-        if (length <= 6) {
-            return "*".repeat(length);
-        }
-        return fnr.substring(0, 6) + "*".repeat(length - 6);
+        return "";
     }
 }
