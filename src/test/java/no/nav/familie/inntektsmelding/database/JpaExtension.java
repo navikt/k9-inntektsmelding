43e55285981e9cc70a7f2ec66821d204219c3427
oyvemb <oyvind.andre.hagen.emblemsvag@nav.no>
2026-07-09 14:15:26 +0200

    Fikser db-oppsett med ny fp-felles

--- src/test/java/no/nav/familie/inntektsmelding/database/JpaExtension.java
+++ src/test/java/no/nav/familie/inntektsmelding/database/JpaExtension.java
@@ -1,19 +1,29 @@
 package no.nav.familie.inntektsmelding.database;
 
-import org.testcontainers.containers.PostgreSQLContainer;
+
+import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 import no.nav.foreldrepenger.konfig.Environment;
+import no.nav.vedtak.felles.jpa.NamingStandard;
+import no.nav.vedtak.felles.jpa.jdbc.DataSourceHolder;
 import no.nav.vedtak.felles.testutilities.db.EntityManagerAwareExtension;
+import no.nav.vedtak.felles.testutilities.db.MigrationUtil;
 
 public class JpaExtension extends EntityManagerAwareExtension {
     private static final String TEST_DB_CONTAINER = Environment.current().getProperty("testcontainer.test.db", String.class, "postgres:17-alpine");
-    private static final PostgreSQLContainer TEST_DATABASE;
 
     static {
-        TEST_DATABASE = new PostgreSQLContainer<>(DockerImageName.parse(TEST_DB_CONTAINER))
-            .withReuse(true);
-        TEST_DATABASE.start();
-        TestDatabaseInit.settOppDatasourceOgMigrer(TEST_DATABASE.getJdbcUrl(), TEST_DATABASE.getUsername(), TEST_DATABASE.getPassword());
+        initDatabase();
+    }
+
+    private static synchronized void initDatabase() {
+        if (!DataSourceHolder.isInitialized()) {
+            var testDatabase = new PostgreSQLContainer(DockerImageName.parse(TEST_DB_CONTAINER)).withReuse(true);
+            testDatabase.start();
+            var dataSource = MigrationUtil.createLocalBuildTestDataSource(testDatabase.getJdbcUrl(), testDatabase.getUsername(), testDatabase.getPassword());
+            MigrationUtil.migrateLocalBuildTest(dataSource, "classpath:/db/postgres/" + NamingStandard.DEFAULT_DATA_SOURCE);
+            DataSourceHolder.initialize(dataSource);
+        }
     }
 }
