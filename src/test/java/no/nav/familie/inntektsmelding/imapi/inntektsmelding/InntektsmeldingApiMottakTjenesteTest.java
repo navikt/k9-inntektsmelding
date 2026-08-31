30e176def099c65c5596ab67e851f594bd4a93d9
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2026-08-31 12:42:41 +0200

    Bedre test

--- src/test/java/no/nav/familie/inntektsmelding/imapi/inntektsmelding/InntektsmeldingApiMottakTjenesteTest.java
+++ src/test/java/no/nav/familie/inntektsmelding/imapi/inntektsmelding/InntektsmeldingApiMottakTjenesteTest.java
@@ -3,6 +3,7 @@ package no.nav.familie.inntektsmelding.imapi.inntektsmelding;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
@@ -279,6 +280,8 @@ class InntektsmeldingApiMottakTjenesteTest {
 
         assertThat(response.success()).isFalse();
         assertThat(response.feilinformasjon().feilkode()).isEqualTo(FeilkodeDto.ULIK_INNTEKT);
+        verify(inntektsmeldingRepository, never()).lagreInntektsmelding(any());
+        verifyNoInteractions(prosessTaskTjeneste);
     }
 
     @Test
