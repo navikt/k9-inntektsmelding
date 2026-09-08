0fc17f988ae1b600060086e687b9633dd912b5fc
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2026-09-08 15:57:43 +0200

    Fjern kommentar

--- src/main/java/no/nav/familie/inntektsmelding/imapi/inntektsmelding/InntektsmeldingApiMottakTjeneste.java
+++ src/main/java/no/nav/familie/inntektsmelding/imapi/inntektsmelding/InntektsmeldingApiMottakTjeneste.java
@@ -285,7 +285,6 @@ public class InntektsmeldingApiMottakTjeneste {
     }
 
     private void settForrigeUtdatertHvisVenterVurdering(ForespørselEntitet forespørsel) {
-        // burde lage en felles kode for å hente siste inntektsmelding
         forespørsel.getInntektsmeldinger().stream()
             .max(java.util.Comparator.comparing(InntektsmeldingEntitet::getOpprettetTidspunkt))
             .filter(im -> InntektsmeldingStatus.VENTER_VURDERING.equals(im.getStatus()))
